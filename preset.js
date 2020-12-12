d33fb4612d36780cd3302e89ac8e907fdf3cc179
Pouya <pouya.saadeghi@gmail.com>
2020-12-13 00:55:13 +0330

    move preset.js to /preset

--- preset.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    require('./preset/colors'),
-    require('./preset/typography'),
-    require('./preset/borderRadius'),
-    require('./preset/fontSize'),
-  ],
-}
---
+++ preset/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    require('./colors'),
+    require('./typography'),
+    require('./borderRadius'),
+    require('./fontSize'),
+  ],
+}
