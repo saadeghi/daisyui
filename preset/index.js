76fd614581a75fc84f0232c5dc5fc501aabd9817
Pouya <pouya.saadeghi@gmail.com>
2020-12-13 01:44:49 +0330

    btn size

--- preset/index.js
+++ preset/index.js
@@ -4,5 +4,6 @@ module.exports = {
     require('./typography'),
     require('./borderRadius'),
     require('./fontSize'),
+    require('./minHeight'),
   ],
 }
---
+++ preset/minHeight.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      minHeight: {
+        '6': '1.5rem',
+        '8': '2rem',
+        '12': '3rem',
+        '16': '4rem',
+      },
+    },
+  },
+}
