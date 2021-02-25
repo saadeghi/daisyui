934c6cd47e4b48fdc2de9c7d0c04ae7f5333741b
Pouya <pouya.saadeghi@gmail.com>
2021-02-25 14:48:00 +0330

    add border radius variants

--- src/tailwind/utilities/borderRadius.js
+++ src/tailwind/utilities/borderRadius.js
@@ -1,5 +1,7 @@
 module.exports = {
   '.rounded-box': { borderRadius: 'var(--rounded-box, 1rem)' },
+  '.rounded-t-box': { borderTopRightRadius: 'var(--rounded-box, 1rem)', borderTopLeftRadius: 'var(--rounded-box, 1rem)' },
+  '.rounded-b-box': { borderBottomRightRadius: 'var(--rounded-box, 1rem)', borderBottomLeftRadius: 'var(--rounded-box, 1rem)' },
   '.rounded-btn': { borderRadius: 'var(--rounded-btn, 0.5rem)' },
   '.rounded-badge': { borderRadius: 'var(--rounded-badge, 9999px)' },
 }
