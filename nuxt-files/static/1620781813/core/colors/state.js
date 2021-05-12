19c8608a0ae9073f704bcb4af0335fd18dfbefb2
saadeghi <saadeghi@users.noreply.github.com>
2021-05-12 01:34:59 +0000

    deploy: 10ea5d02d8cbaa779ef40c19e36cb9bd94ff4c29

--- nuxt-files/static/1620781813/core/colors/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-files\u002Fstatic\u002F1620781813",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcore\u002Fcolors",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
