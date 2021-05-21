1a49d2705e55ae0d81774a2f1dd8b49f60f72fcd
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-05-22 04:05:19 +0430

    update avatar demo

--- src/docs/pages/components/avatar.vue
+++ src/docs/pages/components/avatar.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
     <Wrapper title="avatar" classes="flex items-center space-x-8 flex-wrap">
-      <Avatar classes="mb-8 rounded-btn w-32 h-32">
-        <img src="https://i.pravatar.cc/500?img=32" />
-      </Avatar>
       <Avatar classes="mb-8 rounded-btn w-24 h-24">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar classes="mb-8 rounded-btn w-16 h-16">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 rounded-btn w-14 h-14">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@56w.png">
       </Avatar>
       <Avatar classes="mb-8 rounded-btn w-10 h-10">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@40w.png">
       </Avatar>
     </Wrapper>
 
@@ -27,7 +24,7 @@
           <img src="http://daisyui.com/tailwind-css-component-profile-3@56w.png">
         </Avatar>
         <Avatar classes="w-16 h-16">
-          <img src="http://daisyui.com/tailwind-css-component-profile-4@56w.png">
+          <img src="http://daisyui.com/tailwind-css-component-profile-5@56w.png">
         </Avatar>
       </div>
     </Wrapper>
@@ -44,7 +41,7 @@
           <img src="http://daisyui.com/tailwind-css-component-profile-3@40w.png">
         </Avatar>
         <Avatar classes="w-10 h-10">
-          <img src="http://daisyui.com/tailwind-css-component-profile-4@40w.png">
+          <img src="http://daisyui.com/tailwind-css-component-profile-5@40w.png">
         </Avatar>
         <div class="avatar placeholder">
           <div class="bg-neutral-focus text-neutral-content rounded-full w-10 h-10">
@@ -56,88 +53,73 @@
 
 
     <Wrapper title="avatar circle" classes="flex items-center space-x-8 flex-wrap">
-      <Avatar classes="mb-8 rounded-full w-32 h-32">
-        <img src="https://i.pravatar.cc/500?img=32" />
-      </Avatar>
       <Avatar classes="mb-8 rounded-full w-24 h-24">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar classes="mb-8 rounded-full w-16 h-16">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 rounded-full w-14 h-14">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@56w.png">
       </Avatar>
       <Avatar classes="mb-8 rounded-full w-10 h-10">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@40w.png">
       </Avatar>
     </Wrapper>
 
     <Wrapper title="avatar with mask" classes="flex items-center space-x-8 flex-wrap">
-      <Avatar classes="mb-8 w-32 h-32 mask mask-squircle">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 w-24 h-24 mask mask-squircle">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar classes="mb-8 w-32 h-32 mask mask-hexagon">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 w-24 h-24 mask mask-hexagon">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar classes="mb-8 w-32 h-32 mask mask-decagon">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 w-24 h-24 mask mask-decagon">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
     </Wrapper>
 
     <Wrapper title="avatar with border" classes="flex items-center space-x-8 flex-wrap">
-      <Avatar classes="mb-8 rounded-btn w-32 h-32 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 rounded-box w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar classes="mb-8 rounded-btn w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 rounded-box w-14 h-14 ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@56w.png">
       </Avatar>
-      <Avatar classes="mb-8 rounded-btn w-16 h-16 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
-      </Avatar>
-      <Avatar classes="mb-8 rounded-btn w-10 h-10 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 rounded-box w-10 h-10 ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@40w.png">
       </Avatar>
     </Wrapper>
 
     <Wrapper title="avatar circle" classes="flex items-center space-x-8 flex-wrap">
-      <Avatar classes="mb-8 rounded-full w-32 h-32 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
-      </Avatar>
       <Avatar classes="mb-8 rounded-full w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar classes="mb-8 rounded-full w-16 h-16 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar classes="mb-8 rounded-full w-14 h-14 ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@56w.png">
       </Avatar>
       <Avatar classes="mb-8 rounded-full w-10 h-10 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@40w.png">
       </Avatar>
     </Wrapper>
 
     <Wrapper title="avatar with presense indicator" classes="flex items-center space-x-8 flex-wrap">
-      <Avatar class="offline" classes="rounded-btn w-32 h-32">
-        <img src="https://i.pravatar.cc/500?img=32" />
-      </Avatar>
       <Avatar class="online" classes="rounded-btn w-24 h-24">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar class="offline" classes="rounded-btn w-16 h-16">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar class="offline" classes="rounded-btn w-14 h-14">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@56w.png">
       </Avatar>
       <Avatar class="online" classes="rounded-btn w-10 h-10">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@40w.png">
       </Avatar>
     </Wrapper>
     <Wrapper title="avatar with presense indicator" classes="flex items-center space-x-8 flex-wrap">
-      <Avatar class="offline" classes="rounded-full w-32 h-32">
-        <img src="https://i.pravatar.cc/500?img=32" />
-      </Avatar>
       <Avatar class="online" classes="rounded-full w-24 h-24">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
       </Avatar>
-      <Avatar class="offline" classes="rounded-full w-16 h-16">
-        <img src="https://i.pravatar.cc/500?img=32" />
+      <Avatar class="offline" classes="rounded-full w-14 h-14">
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@56w.png">
       </Avatar>
       <Avatar class="online" classes="rounded-full w-10 h-10">
-        <img src="https://i.pravatar.cc/500?img=32" />
+        <img src="http://daisyui.com/tailwind-css-component-profile-1@40w.png">
       </Avatar>
     </Wrapper>
     <Wrapper title="avatar placeholder" classes="flex items-center space-x-8 flex-wrap">
