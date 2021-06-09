c2d8d7fa0aa533cddc1b1120167a7123c9a77f9a
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-06-10 04:24:32 +0430

    docs: scroll to top when page changes

--- src/docs/layouts/default.vue
+++ src/docs/layouts/default.vue
@@ -3,7 +3,7 @@
   <Header/>
   <div class="drawer drawer-mobile">
     <input id="main-menu" type="checkbox" class="drawer-toggle" v-model="showMainMenu" >
-    <main class="flex-grow block pt-16 overflow-x-hidden bg-base-100 text-base-content drawer-content">
+    <main class="flex-grow block pt-16 overflow-x-hidden bg-base-100 text-base-content drawer-content" ref="drawerContent">
       <div class="p-4 lg:p-10">
         <Nuxt />
       </div>
@@ -174,6 +174,11 @@ export default {
         })
       }
     })
+  },
+  watch: {
+    $route () {
+      this.$refs.drawerContent.scrollTop = 0;
+    }
   },
   mounted(){
     themeChange(false)
