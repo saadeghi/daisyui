e1012b7b080b3db678f1e40b574d8e174657f449
saadeghi <saadeghi@users.noreply.github.com>
2021-06-19 00:44:49 +0000

    deploy: 4e4f7fa95e0c50edab78b5f089bd9be46beeb9e3

--- nuxt-files/static/1624000452/components/collapse/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-files\u002Fstatic\u002F1624000452",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcomponents\u002Fcollapse",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
