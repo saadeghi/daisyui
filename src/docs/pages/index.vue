d4ef81dfae0d4d989d65a67924b8c88b0c4c0c94
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-07-29 01:21:02 +0430

    update stats

--- src/docs/pages/index.vue
+++ src/docs/pages/index.vue
@@ -12,20 +12,20 @@
 
       <div class="flex justify-center w-full -mt-56">
         <div class="stats">
-          <NuxtLink class="stat px-10 place-items-center gap-0 bg-transparent hover:opacity-70" to="/components/button">
-            <div class="stat-value">35</div>
+          <NuxtLink class="gap-0 px-10 bg-transparent stat place-items-center hover:opacity-70" to="/components/button">
+            <div class="stat-value">36</div>
             <div class="stat-desc">Components</div>
           </NuxtLink>
-          <NuxtLink class="stat px-10 place-items-center gap-0 bg-transparent hover:opacity-70" to="/docs/default-themes">
+          <NuxtLink class="gap-0 px-10 bg-transparent stat place-items-center hover:opacity-70" to="/docs/default-themes">
             <div class="stat-value">21</div>
             <div class="stat-desc">Themes</div>
           </NuxtLink>
-          <a class="stat px-10 place-items-center gap-0 bg-transparent hover:opacity-70" href="https://github.com/saadeghi/daisyui" target="_blank" rel="noopener">
-            <div class="stat-value">2,000</div>
+          <a class="gap-0 px-10 bg-transparent stat place-items-center hover:opacity-70" href="https://github.com/saadeghi/daisyui" target="_blank" rel="noopener">
+            <div class="stat-value">2,500</div>
             <div class="stat-desc">Github Stars</div>
           </a>
-          <a class="stat px-10 place-items-center gap-0 bg-transparent hover:opacity-70" href="https://www.npmjs.com/package/daisyui" target="_blank" rel="noopener">
-            <div class="stat-value">50,000</div>
+          <a class="gap-0 px-10 bg-transparent stat place-items-center hover:opacity-70" href="https://www.npmjs.com/package/daisyui" target="_blank" rel="noopener">
+            <div class="stat-value">60,000</div>
             <div class="stat-desc">NPM Installs</div>
           </a>
         </div>
