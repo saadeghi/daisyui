f3d9901b57df46422a60988aec795895b427f6e3
saadeghi <saadeghi@users.noreply.github.com>
2021-08-22 21:10:42 +0000

    deploy: f69d55833634cdf95742a03ff1ec64bb579c9e1b

---
+++ nuxt-files/static/1629666632/compare/Bulma-alternative/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-files\u002Fstatic\u002F1629666632",layout:"compare",error:a,serverRendered:true,routePath:"\u002Fcompare\u002FBulma-alternative",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
