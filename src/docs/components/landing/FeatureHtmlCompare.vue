0500b529a190199183a1939fd4a6fba78e38c479
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-10-01 03:42:19 +0300

    docs: add toggle example to homepage

--- src/docs/components/landing/FeatureHtmlCompare.vue
+++ src/docs/components/landing/FeatureHtmlCompare.vue
@@ -8,7 +8,7 @@
         </h2>
         <h3 class="mb-5 text-3xl font-bold">with component classes</h3>
         <p class="w-full max-w-lg mx-auto mb-5">
-          Your HTML doesn't need to be messy. DaisyUI adds component classes to
+          Your HTML doesn't need to be messy. daisyUI adds component classes to
           Tailwind CSS. Classes like
           <span class="badge badge-outline">btn</span>,
           <span class="badge badge-outline">card</span>, etc... No need to deal
@@ -17,7 +17,7 @@
 
 
         <p class="w-full max-w-lg mx-auto mt-20">
-          Creating a button <br>using only utility classes vs using DaisyUI component classes
+          <div class="font-bold">Creating a button </div>using only utility classes vs using daisyUI component classes
         </p>
 
         <div class="flex flex-col mt-10 mb-20 lg:flex-row">
@@ -57,9 +57,54 @@
         </div>
 
 
+        <p class="w-full max-w-lg mx-auto mt-20">
+          <div class="font-bold">Creating a toggle </div>using only utility classes vs using daisyUI component classes
+        </p>
+
+        <div class="flex flex-col mt-10 mb-20 lg:flex-row">
+          <div class="flex flex-col w-full text-left">
+            <div class="flex-grow w-full max-w-xs mx-auto shadow-lg mockup-code sm:max-w-md">
+              <pre><code>&#x3C;div class=&#x22;<span class="text-warning">relative</span>&#x22;&#x3E;
+      &#x3C;input type=&#x22;checkbox&#x22; class=&#x22;<span class="text-warning">peer
+      appearance-none cursor-pointer
+      border border-gray-300 rounded-full
+      checked:border-gray-900 w-12 h-7</span>&#x22;/&#x3E;
+      &#x3C;span class=&#x22;<span class="text-warning">peer-checked:left-6
+      peer-checked:bg-gray-900
+      transition-all duration-500
+      pointer-events-none w-5 h-5
+      block absolute top-1 left-1
+      rounded-full bg-gray-300</span>&#x22;&#x3E;&#x3C;/span&#x3E;
+    &#x3C;/div&#x3E;</code></pre>
+            </div>
+            <div class="flex items-start justify-center h-16 mt-4">
+              <div class="relative">
+                <input type="checkbox" class="w-12 border border-gray-300 rounded-full appearance-none cursor-pointer peer checked:border-gray-900 h-7" checked />
+                <span class="absolute block w-5 h-5 transition-all duration-500 bg-gray-300 rounded-full pointer-events-none peer-checked:left-6 peer-checked:bg-gray-900 top-1 left-1"></span>
+              </div>
+            </div>
+          </div>
+
+          <div class="hidden divider lg:flex divider-vertical text-base-content text-opacity-30">
+            VS
+          </div>
+          <div class="divider lg:hidden text-base-content text-opacity-30">
+            VS
+          </div>
+          <div class="flex flex-col w-full text-left">
+            <div class="flex-grow w-full max-w-xs mx-auto shadow-lg mockup-code sm:max-w-md">
+              <pre><code>&#x3C;input type="checkbox" class=&#x22;<span class="text-info">toggle</span>&#x22;/&#x3E;</code></pre>
+            </div>
+            <div class="flex items-start justify-center h-16 mt-4">
+              <input type="checkbox" class="toggle" checked />
+            </div>
+          </div>
+        </div>
+
+
 
         <p class="w-full max-w-lg mx-auto">
-          Creating a menu component <br>using only utility classes vs using DaisyUI component classes
+          <div class="font-bold">Creating a menu component </div>using only utility classes vs using daisyUI component classes
         </p>
 
 
@@ -95,7 +140,7 @@
           </div>
           <div class="flex flex-col w-full text-left">
             <div class="flex-grow w-full max-w-xs mx-auto shadow-lg mockup-code sm:max-w-md">
-              <pre><code>&lt;ul class=&quot;<span class="text-info">menu</span> p-3 bg-base-200 rounded-xl&quot;&gt;
+              <pre class="pr-4"><code>&lt;ul class=&quot;<span class="text-info">menu</span> p-3 bg-base-200 rounded-xl&quot;&gt;
       &lt;li class=&quot;<span class="text-info">menu-title</span>&quot;&gt;
         &lt;span&gt;Menu Title&lt;/span&gt;
       &lt;/li&gt;
@@ -116,7 +161,11 @@
         </div>
 
 
-
+      <div class="flex justify-center w-full mt-6 mb-10">
+        <NuxtLink to="/components/button" class="btn btn-primary btn-wide">
+          See All Components
+        </NuxtLink>
+      </div>
 
 
 
@@ -125,3 +174,16 @@
     </div>
   </div>
 </template>
+<style>
+.checked\:border-gray-900:checked {
+  --tw-border-opacity: 1;
+  border-color: rgba(17, 24, 39, var(--tw-border-opacity));
+}
+.peer:checked ~ .peer-checked\:bg-gray-900 {
+  --tw-bg-opacity: 1;
+  background-color: rgba(17, 24, 39, var(--tw-bg-opacity));
+}
+.peer:checked ~ .peer-checked\:left-6 {
+  left: 1.5rem;
+}
+</style>
