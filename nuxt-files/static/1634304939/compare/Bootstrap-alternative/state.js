624effee830e4a4bac1f021bebdaf56a8af75826
saadeghi <saadeghi@users.noreply.github.com>
2021-10-15 13:35:52 +0000

    deploy: 5796d7c4d6d017d82855544d322af03ef74dcbd2

---
+++ nuxt-files/static/1634304939/compare/Bootstrap-alternative/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-files\u002Fstatic\u002F1634304939",layout:"compare",error:a,serverRendered:true,routePath:"\u002Fcompare\u002FBootstrap-alternative",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
