6d35d7c1aa84557fee0e71a0405dbf8a1e17f4f2
saadeghi <saadeghi@users.noreply.github.com>
2022-01-16 11:24:17 +0000

    deploy: d249bd349a26c4b7ba419bd848c6f464cea6da89

---
+++ nuxt-files/static/1642332245/core/layout/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-files\u002Fstatic\u002F1642332245",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcore\u002Flayout",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
