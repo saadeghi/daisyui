fd9cb711b575c78f99480b960cd3dd376ba525b8
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-02-08 20:39:34 +0300

    sidebar bg color and z-index

--- src/docs2/src/components/Sidebar.svelte
+++ src/docs2/src/components/Sidebar.svelte
@@ -8,7 +8,7 @@
   $: switchNavbarStyle = drawerSidebarScrollY > 40 ? true : false
 </script>
 
-<div class={`bg-base-200 bg-opacity-90 backdrop-blur sticky top-0 items-center gap-2 px-4 py-2 hidden ${$page.path == "/" ? "" : "lg:flex"} ${switchNavbarStyle ? "shadow-sm" : ""}`}>
+<div class={`z-20 bg-base-200 bg-opacity-90 backdrop-blur sticky top-0 items-center gap-2 px-4 py-2 hidden ${$page.path == "/" ? "" : "lg:flex"} ${switchNavbarStyle ? "shadow-sm" : ""}`}>
   <a href="/" aria-current="page" aria-label="Homepage" class="flex-0 btn btn-ghost active nuxt-link-active px-2">
     <div class="font-title text-primary inline-flex text-lg transition-all duration-200 md:text-3xl">
       <span class="lowercase">daisy</span>
@@ -23,7 +23,7 @@
 </div>
 
 {#each menu as { name, items }}
-  <ul class="menu menu-compact flex flex-col p-2 px-4">
+  <ul class="menu menu-compact bg-base-200 flex flex-col p-2 px-4">
     <li class="menu-title"><span>{name}</span></li>
     {#each items as { name, href, icon, badge }}
       <li>
