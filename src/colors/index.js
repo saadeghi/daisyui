f2a7ff50b28d4667fd38aa142e16c830ac5be66f
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-02-12 21:31:12 +0300

    add fallback colors

--- src/colors/index.js
+++ src/colors/index.js
@@ -1,17 +1,53 @@
-const colorValues = require('./colorNames')
+
+function withOpacityValue(variable, fallbackColor) {
+  return ({ opacityValue }) => {
+    let fallbackColorValue = ''
+    if (fallbackColor) {
+      fallbackColorValue = `, var(${fallbackColor})`
+    }
+    if (opacityValue === undefined) {
+      return `hsl(var(${variable}${fallbackColorValue}))`
+    }
+    return `hsl(var(${variable}${fallbackColorValue}) / ${opacityValue})`
+  }
+}
 
 let colorObject = {
   "transparent": "transparent",
   "current": "currentColor",
-}
 
-for (const [key, item] of Object.entries(colorValues)) {
-  colorObject[key] = ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `hsl(var(`+ item +`))`
-    }
-    return `hsl(var(`+ item +`) / ${opacityValue})`
-  }
+  "primary": withOpacityValue("--p"),
+  "primary-focus": withOpacityValue("--pf", "--p"),
+  "primary-content": withOpacityValue("--pc"),
+
+  "secondary": withOpacityValue("--s"),
+  "secondary-focus": withOpacityValue("--sf", "--s"),
+  "secondary-content": withOpacityValue("--sc"),
+
+  "accent": withOpacityValue("--a"),
+  "accent-focus": withOpacityValue("--af", "--a"),
+  "accent-content": withOpacityValue("--ac"),
+
+  "neutral": withOpacityValue("--n"),
+  "neutral-focus": withOpacityValue("--nf", "--n"),
+  "neutral-content": withOpacityValue("--nc"),
+
+  "base-100": withOpacityValue("--b1"),
+  "base-200": withOpacityValue("--b2", "--b1"),
+  "base-300": withOpacityValue("--b3", "--b2"),
+  "base-content": withOpacityValue("--bc"),
+
+  "info": withOpacityValue("--in"),
+  "info-content": withOpacityValue("--inc", "--nc"),
+
+  "success": withOpacityValue("--su"),
+  "success-content": withOpacityValue("--suc", "--nc"),
+
+  "warning": withOpacityValue("--wa"),
+  "warning-content": withOpacityValue("--wac", "--nc"),
+
+  "error": withOpacityValue("--er"),
+  "error-content": withOpacityValue("--erc", "--nc"),
 }
 
 module.exports = colorObject
