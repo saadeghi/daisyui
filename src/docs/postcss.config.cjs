9c4796c36326739a053947e1232e2b835ccc8a66
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-02-13 13:32:22 +0300

    test deploy

--- src/docs/postcss.config.cjs
+++ src/docs/postcss.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     (process.env.NODE_ENV === 'production') ? null : require('postcss-import'),
-    (process.env.NODE_ENV === 'production') ? null : require('tailwindcss/nesting'),
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
   ]
