43935497395df4a650a5b72404abf88705ea780f
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-02-20 00:37:33 +0300

    docs

--- src/docs/src/lib/data.js
+++ src/docs/src/lib/data.js
@@ -47,7 +47,7 @@ export let pages = [
         tags: "themes disable custom color generator",
         href: "/docs/themes",
         icon: '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" /></svg>',
-        badge: "",
+        badge: "updated",
       },
       {
         name: "Colors",
