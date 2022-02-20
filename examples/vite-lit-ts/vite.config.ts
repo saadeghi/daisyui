4a46a9c12d555da215753dfe1926348a7fda0548
txfs19260817 <duola865@gmail.com>
2022-02-20 11:47:56 -0800

    docs: add a vite-lit-ts example

---
+++ examples/vite-lit-ts/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import litcss from 'rollup-plugin-lit-css';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/my-element.ts',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
+});
