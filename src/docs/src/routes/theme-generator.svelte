6abf7da209f159b6b630303843d0824f59e430dc
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-03-03 19:15:05 +0300

    docs: fix color

--- src/docs/src/routes/theme-generator.svelte
+++ src/docs/src/routes/theme-generator.svelte
@@ -473,7 +473,7 @@
       <div class="flex flex-col gap-3">
         <div class="alert">
           <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info-content h-6 w-6 flex-shrink-0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info h-6 w-6 flex-shrink-0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>12 unread messages. Tap to see.</span>
           </div>
         </div>
