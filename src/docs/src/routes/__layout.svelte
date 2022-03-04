fc1d88631a8239c0b112b0ee1dba12ec0a35b695
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-03-04 15:25:43 +0300

    font optimize

--- src/docs/src/routes/__layout.svelte
+++ src/docs/src/routes/__layout.svelte
@@ -69,7 +69,7 @@
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap&text=daisyUIThemostpopular,freeandopen-sourceTailwindCSScomponentlibrary" rel="stylesheet" />
 </svelte:head>
 
 {#if post}
