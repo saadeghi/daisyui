d3bb283176feadb14d8709117d0d4a9659a22152
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-03-04 12:43:04 +0300

    docs

--- src/docs/src/routes/__layout.svelte
+++ src/docs/src/routes/__layout.svelte
@@ -69,7 +69,7 @@
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap&text=daisyUITailwindCSSComponents" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
 </svelte:head>
 
 {#if post}
