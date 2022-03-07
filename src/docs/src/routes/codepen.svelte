96567de3d59dcf00ae239115b7b4b590df08261f
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-03-07 13:26:08 +0300

    update codepan demo URL

--- src/docs/src/routes/codepen.svelte
+++ src/docs/src/routes/codepen.svelte
@@ -2,7 +2,7 @@
   export async function load() {
     return {
       status: 302,
-      redirect: "https://codepen.io/saadeghi/pen/gOwWKvv",
+      redirect: "https://codepen.io/pen?template=gOwWKvv",
     }
   }
 </script>
@@ -10,6 +10,6 @@
 <script>
   import { onMount } from "svelte"
   onMount(() => {
-    window.location.href = "https://codepen.io/saadeghi/pen/gOwWKvv"
+    window.location.href = "https://codepen.io/pen?template=gOwWKvv"
   })
 </script>
