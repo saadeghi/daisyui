6be8da119c7efa696c56b2b4df02f40a2af24837
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-03-17 01:56:17 +0300

    improve theme picker

--- src/docs/src/components/ThemeChange.svelte
+++ src/docs/src/components/ThemeChange.svelte
@@ -1,5 +1,7 @@
 <script>
   import { onMount } from "svelte"
+  import { themes } from "@src/lib/data.js"
+
   import { themeChange } from "theme-change"
   import { t } from "@src/lib/i18n"
   onMount(() => {
@@ -10,48 +12,52 @@
   export let contentClasses = "mt-16"
 </script>
 
-<!-- <div class="overflow-hidden btn btn-sm btn-circle btn-ghost" data-toggle-theme="dark" data-act-class="group">
-  <svg class="m-[.5em] w-4 h-4 fill-current group-first:ml-[-1.7em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256,118a22,22,0,0,1-22-22V48a22,22,0,0,1,44,0V96A22,22,0,0,1,256,118Z"/><path d="M256,486a22,22,0,0,1-22-22V416a22,22,0,0,1,44,0v48A22,22,0,0,1,256,486Z"/><path d="M369.14,164.86a22,22,0,0,1-15.56-37.55l33.94-33.94a22,22,0,0,1,31.11,31.11l-33.94,33.94A21.93,21.93,0,0,1,369.14,164.86Z"/><path d="M108.92,425.08a22,22,0,0,1-15.55-37.56l33.94-33.94a22,22,0,1,1,31.11,31.11l-33.94,33.94A21.94,21.94,0,0,1,108.92,425.08Z"/><path d="M464,278H416a22,22,0,0,1,0-44h48a22,22,0,0,1,0,44Z"/><path d="M96,278H48a22,22,0,0,1,0-44H96a22,22,0,0,1,0,44Z"/><path d="M403.08,425.08a21.94,21.94,0,0,1-15.56-6.45l-33.94-33.94a22,22,0,0,1,31.11-31.11l33.94,33.94a22,22,0,0,1-15.55,37.56Z"/><path d="M142.86,164.86a21.89,21.89,0,0,1-15.55-6.44L93.37,124.48a22,22,0,0,1,31.11-31.11l33.94,33.94a22,22,0,0,1-15.56,37.55Z"/><path d="M256,358A102,102,0,1,1,358,256,102.12,102.12,0,0,1,256,358Z"/></svg>
-  <svg class="m-[.5em] w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M152.62,126.77c0-33,4.85-66.35,17.23-94.77C87.54,67.83,32,151.89,32,247.38,32,375.85,136.15,480,264.62,480c95.49,0,179.55-55.54,215.38-137.85-28.42,12.38-61.8,17.23-94.77,17.23C256.76,359.38,152.62,255.24,152.62,126.77Z"/></svg>
-</div> -->
-
 <div title="Change Theme" class={`dropdown dropdown-end ${dropdownClasses}`}>
   <div tabindex="0" class={`btn gap-1 normal-case ${btnClasses}`}>
     <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" /></svg>
     <span class="hidden md:inline">{$t("change-theme-btn")}</span>
     <svg width="12px" height="12px" class="ml-1 hidden h-3 w-3 fill-current opacity-60 sm:inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z" /></svg>
   </div>
-  <div class={`dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px h-96 w-52 overflow-y-auto shadow-2xl ${contentClasses}`}>
-    <ul class="menu menu-compact p-2" tabindex="0">
-      <li><button data-set-theme="light" data-act-class="active" class="active">🌝  light</button></li>
-      <li><button data-set-theme="dark" data-act-class="active">🌚  dark</button></li>
-      <li><button data-set-theme="cupcake" data-act-class="active">🧁  cupcake</button></li>
-      <li><button data-set-theme="bumblebee" data-act-class="active">🐝  bumblebee</button></li>
-      <li><button data-set-theme="emerald" data-act-class="active">✳️  Emerald</button></li>
-      <li><button data-set-theme="corporate" data-act-class="active">🏢  Corporate</button></li>
-      <li><button data-set-theme="synthwave" data-act-class="active">🌃  synthwave</button></li>
-      <li><button data-set-theme="retro" data-act-class="active">👴  retro</button></li>
-      <li><button data-set-theme="cyberpunk" data-act-class="active">🤖  cyberpunk</button></li>
-      <li><button data-set-theme="valentine" data-act-class="active">🌸  valentine</button></li>
-      <li><button data-set-theme="halloween" data-act-class="active">🎃  halloween</button></li>
-      <li><button data-set-theme="garden" data-act-class="active">🌷  garden</button></li>
-      <li><button data-set-theme="forest" data-act-class="active">🌲  forest</button></li>
-      <li><button data-set-theme="aqua" data-act-class="active">🐟  aqua</button></li>
-      <li><button data-set-theme="lofi" data-act-class="active">👓  lofi</button></li>
-      <li><button data-set-theme="pastel" data-act-class="active">🖍  pastel</button></li>
-      <li><button data-set-theme="fantasy" data-act-class="active">🧚‍♀️  fantasy</button></li>
-      <li><button data-set-theme="wireframe" data-act-class="active">📝  Wireframe</button></li>
-      <li><button data-set-theme="black" data-act-class="active">🏴  black</button></li>
-      <li><button data-set-theme="luxury" data-act-class="active">💎  luxury</button></li>
-      <li><button data-set-theme="dracula" data-act-class="active">🧛‍♂️  dracula</button></li>
-      <li><button data-set-theme="cmyk" data-act-class="active">🖨  CMYK</button></li>
-      <li><button data-set-theme="autumn" data-act-class="active">🍁  Autumn</button></li>
-      <li><button data-set-theme="business" data-act-class="active">💼  Business</button></li>
-      <li><button data-set-theme="acid" data-act-class="active">💊  Acid</button></li>
-      <li><button data-set-theme="lemonade" data-act-class="active">🍋  Lemonade</button></li>
-      <li><button data-set-theme="night" data-act-class="active">🌙  Night</button></li>
-      <li><button data-set-theme="coffee" data-act-class="active">☕️  Coffee</button></li>
-      <li><button data-set-theme="winter" data-act-class="active">❄️  Winter</button></li>
-    </ul>
+  <div class={`dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl ${contentClasses}`}>
+    <div class="grid grid-cols-1 gap-3 p-3" tabindex="0">
+      {#each themes as theme}
+        <div class="outline-base-content overflow-hidden rounded-lg outline outline-2 outline-offset-2" data-set-theme={theme.id} data-act-class="outline">
+          <div data-theme={theme.id} class="bg-base-100 text-base-content w-full cursor-pointer font-sans">
+            <div class="grid grid-cols-5 grid-rows-3">
+              <div class="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
+                <div class="flex-grow text-sm font-bold">
+                  {theme.id}
+                </div>
+                <div class="flex flex-shrink-0 flex-wrap gap-1">
+                  <div class="bg-primary w-2 rounded" />
+                  <div class="bg-secondary w-2 rounded" />
+                  <div class="bg-accent w-2 rounded" />
+                  <div class="bg-neutral w-2 rounded" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      {/each}
+      <a class="outline-base-content overflow-hidden rounded-lg" href="/theme-generator/">
+        <div class="hover:bg-neutral hover:text-neutral-content w-full cursor-pointer font-sans">
+          <div class="flex gap-2 p-3">
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current" viewBox="0 0 512 512">
+              <path d="M96,208H48a16,16,0,0,1,0-32H96a16,16,0,0,1,0,32Z" />
+              <line x1="90.25" y1="90.25" x2="124.19" y2="124.19" />
+              <path d="M124.19,140.19a15.91,15.91,0,0,1-11.31-4.69L78.93,101.56a16,16,0,0,1,22.63-22.63l33.94,33.95a16,16,0,0,1-11.31,27.31Z" />
+              <path d="M192,112a16,16,0,0,1-16-16V48a16,16,0,0,1,32,0V96A16,16,0,0,1,192,112Z" />
+              <line x1="293.89" y1="90.25" x2="259.95" y2="124.19" />
+              <path d="M260,140.19a16,16,0,0,1-11.31-27.31l33.94-33.95a16,16,0,0,1,22.63,22.63L271.27,135.5A15.94,15.94,0,0,1,260,140.19Z" />
+              <line x1="124.19" y1="259.95" x2="90.25" y2="293.89" />
+              <path d="M90.25,309.89a16,16,0,0,1-11.32-27.31l33.95-33.94a16,16,0,0,1,22.62,22.63l-33.94,33.94A16,16,0,0,1,90.25,309.89Z" />
+              <path d="M219,151.83a26,26,0,0,0-36.77,0l-30.43,30.43a26,26,0,0,0,0,36.77L208.76,276a4,4,0,0,0,5.66,0L276,214.42a4,4,0,0,0,0-5.66Z" />
+              <path d="M472.31,405.11,304.24,237a4,4,0,0,0-5.66,0L237,298.58a4,4,0,0,0,0,5.66L405.12,472.31a26,26,0,0,0,36.76,0l30.43-30.43h0A26,26,0,0,0,472.31,405.11Z" />
+            </svg>
+            <div class="flex-grow text-sm font-bold">{$t("Make your theme")}!</div>
+          </div>
+        </div>
+      </a>
+    </div>
   </div>
 </div>
--- src/docs/src/lib/data.js
+++ src/docs/src/lib/data.js
@@ -15,6 +15,124 @@ export let pagesThatDontNeedSidebar = [
   "/"
 ]
 
+export let themes = [
+  {
+    name: "🌝  light",
+    id: "light",
+  },
+  {
+    name: "🌚  dark",
+    id: "dark",
+  },
+  {
+    name: "🧁  cupcake",
+    id: "cupcake",
+  },
+  {
+    name: "🐝  bumblebee",
+    id: "bumblebee",
+  },
+  {
+    name: "✳️  Emerald",
+    id: "emerald",
+  },
+  {
+    name: "🏢  Corporate",
+    id: "corporate",
+  },
+  {
+    name: "🌃  synthwave",
+    id: "synthwave",
+  },
+  {
+    name: "👴  retro",
+    id: "retro",
+  },
+  {
+    name: "🤖  cyberpunk",
+    id: "cyberpunk",
+  },
+  {
+    name: "🌸  valentine",
+    id: "valentine",
+  },
+  {
+    name: "🎃  halloween",
+    id: "halloween",
+  },
+  {
+    name: "🌷  garden",
+    id: "garden",
+  },
+  {
+    name: "🌲  forest",
+    id: "forest",
+  },
+  {
+    name: "🐟  aqua",
+    id: "aqua",
+  },
+  {
+    name: "👓  lofi",
+    id: "lofi",
+  },
+  {
+    name: "🖍  pastel",
+    id: "pastel",
+  },
+  {
+    name: "🧚‍♀️  fantasy",
+    id: "fantasy",
+  },
+  {
+    name: "📝  Wireframe",
+    id: "wireframe",
+  },
+  {
+    name: "🏴  black",
+    id: "black",
+  },
+  {
+    name: "💎  luxury",
+    id: "luxury",
+  },
+  {
+    name: "🧛‍♂️  dracula",
+    id: "dracula",
+  },
+  {
+    name: "🖨  CMYK",
+    id: "cmyk",
+  },
+  {
+    name: "🍁  Autumn",
+    id: "autumn",
+  },
+  {
+    name: "💼  Business",
+    id: "business",
+  },
+  {
+    name: "💊  Acid",
+    id: "acid",
+  },
+  {
+    name: "🍋  Lemonade",
+    id: "lemonade",
+  },
+  {
+    name: "🌙  Night",
+    id: "night",
+  },
+  {
+    name: "☕️  Coffee",
+    id: "coffee",
+  },
+  {
+    name: "❄️  Winter",
+    id: "winter",
+  },
+]
 
 export let exampleRepos = [
   {
