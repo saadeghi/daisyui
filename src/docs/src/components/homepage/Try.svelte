453af5dcae52e169f714e73d4353fa78d6c908a1
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-03-24 17:10:04 +0300

    docs: fix horizontal scroll on homepage

--- src/docs/src/components/homepage/Try.svelte
+++ src/docs/src/components/homepage/Try.svelte
@@ -20,7 +20,7 @@
         </div>
       </div>
       <p class="mx-auto mt-10 mb-5 max-w-lg">{$t("try-online-playground-links-title")}:</p>
-      <div class="flex justify-center gap-2">
+      <div class="flex flex-wrap justify-center gap-2">
         <a target="_blank" rel="noopener" href="/codepen" class="btn-outline btn gap-3 normal-case">
           <svg class="h-4 w-4 stroke-current" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="2.3" stroke-linecap="round" stroke-linejoin="round"><path d="M3.06 41.732L32 60.932l28.94-19.2V22.268L32 3.068l-28.94 19.2zm57.878 0L32 22.268 3.06 41.732m0-19.463L32 41.47l28.94-19.2M32 3.068v19.2m0 19.463v19.2" stroke-width="6.05" /></svg>
           CodePen
