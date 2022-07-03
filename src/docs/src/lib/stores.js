0903b2b808096cb7f504952ac8c53b9ef6442e60
cryptodeal <jimmydeal16@gmail.com>
2022-07-03 14:49:35 -0500

    remove svelte notification store implementation

--- src/docs/src/lib/stores.js
+++ src/docs/src/lib/stores.js
@@ -1,49 +1,5 @@
-import { writable, derived } from "svelte/store"
+import { writable } from "svelte/store"
 
 // export let store = writable();
 
-export const prefix = writable("")
-
-function createNotificationStore (timeout) {
-  const _notifications = writable([])
-
-  function send (message, type = "default", timeout) {
-      _notifications.update(state => {
-          return [...state, { id: id(), type, message, timeout }]
-      })
-  }
-
-  let timers = []
-
-  const notifications = derived(_notifications, ($_notifications, set) => {
-      set($_notifications)
-      if ($_notifications.length > 0) {
-          const timer = setTimeout(() => {
-              _notifications.update(state => {
-                  state.shift()
-                  return state
-              })
-          }, $_notifications[0].timeout)
-          return () => {
-              clearTimeout(timer)
-          }
-      }
-  })
-  const { subscribe } = notifications
-
-  return {
-      subscribe,
-      send,
-      default: (msg, timeout) => send(msg, "default", timeout),
-      info: (msg, timeout) => send(msg, "info", timeout),
-      success: (msg, timeout) => send(msg, "success", timeout),
-      warning: (msg, timeout) => send(msg, "warning", timeout),
-      error: (msg, timeout) => send(msg, "error", timeout),
-  }
-}
-
-function id() {
-  return '_' + Math.random().toString(36).slice(2, 9);
-};
-
-export const notifications = createNotificationStore()
+export const prefix = writable("");
