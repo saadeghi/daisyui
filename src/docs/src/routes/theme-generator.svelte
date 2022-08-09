a517e3b34ad541113f47a9c2e7eff1c26b68d53e
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-08-09 20:28:30 +0300

    fix translation

--- src/docs/src/routes/theme-generator.svelte
+++ src/docs/src/routes/theme-generator.svelte
@@ -263,7 +263,7 @@
     <div>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info-content h-6 w-6 flex-shrink-0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
       <div>
-        <p><Translate text="You can also define optional colors to have more control on the color values (for example: the color of button when it's focused the color of the text on a button)" /></p>
+        <p><Translate text="You can also define optional colors to have more control on the color values (for example: the color of a button when it's focused on the color of the text on a button)" /></p>
         <p>
           <Translate text="Check out <a class='link font-bold' href='/docs/colors/'>colors page</a> to see all the color names you can use." />
         </p>
