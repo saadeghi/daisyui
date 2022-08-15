7aa377e7ab18fc0e2fabb66247b692de62d0300b
Deyan Petrov <deyanpetrov89@yahoo.com>
2022-08-15 09:17:37 +0200

    fixed: Typo

--- src/tests/colors.test.js
+++ src/tests/colors.test.js
@@ -5,10 +5,10 @@ import { colorToHslValue } from "./utils"
 
 
 test('Foreground color generator function', () => {
-  expect(colorFunctions.generateForegorundColorFrom("#fff")).toBe(colorToHslValue("#333"))
-  expect(colorFunctions.generateForegorundColorFrom("#000")).toBe(colorToHslValue("#ccc"))
-  expect(colorFunctions.generateForegorundColorFrom("#fff", 1)).toBe(colorToHslValue("#000"))
-  expect(colorFunctions.generateForegorundColorFrom("#000", 1)).toBe(colorToHslValue("#fff"))
-  expect(colorFunctions.generateForegorundColorFrom("#1D9BF0")).toBe(colorToHslValue("#d2edff"))
-  expect(colorFunctions.generateForegorundColorFrom("#CF212E")).toBe(colorToHslValue("#ffc7cb"))
+  expect(colorFunctions.generateForegroundColorFrom("#fff")).toBe(colorToHslValue("#333"))
+  expect(colorFunctions.generateForegroundColorFrom("#000")).toBe(colorToHslValue("#ccc"))
+  expect(colorFunctions.generateForegroundColorFrom("#fff", 1)).toBe(colorToHslValue("#000"))
+  expect(colorFunctions.generateForegroundColorFrom("#000", 1)).toBe(colorToHslValue("#fff"))
+  expect(colorFunctions.generateForegroundColorFrom("#1D9BF0")).toBe(colorToHslValue("#d2edff"))
+  expect(colorFunctions.generateForegroundColorFrom("#CF212E")).toBe(colorToHslValue("#ffc7cb"))
 })
