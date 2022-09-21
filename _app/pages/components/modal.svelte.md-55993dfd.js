19dabb21960548aee74e65c30230254e8aee1442
saadeghi <saadeghi@users.noreply.github.com>
2022-09-21 20:06:12 +0000

    deploy: 801fb5b1d776cf5219a428bf41033f2ee022b2d7

---
+++ _app/pages/components/modal.svelte.md-55993dfd.js
@@ -0,0 +1,143 @@
+import{S as O,i as Q,s as U,C as B,w,x as P,y as Y,z as V,A as K,q as C,o as I,B as M,K as X,ag as G,k as T,m as W,g as p,d as c,e as f,t as $,c as h,a as b,h as _,b as u,F as v,a9 as g,W as y}from"../../chunks/vendor-c5cb7521.js";import{M as Z}from"../../chunks/_markdown-37b3bc42.js";import{p as ee,C as te,a as F,r as x}from"../../chunks/actions-04f352c5.js";import"../../chunks/stores-596c3501.js";import"../../chunks/Ads-6b7026d0.js";import"../../chunks/index-fd54b2c0.js";import"../../chunks/SEO-981fc2bd.js";import"../../chunks/preload-helper-ec9aa979.js";import"../../chunks/Translate-02e002d8.js";function le(d){let e,n;return{c(){e=f("label"),n=$("open modal"),this.h()},l(o){e=h(o,"LABEL",{for:!0,class:!0});var l=b(e);n=_(l,"open modal"),l.forEach(c),this.h()},h(){u(e,"for","my-modal"),u(e,"class","btn modal-button")},m(o,l){p(o,e,l),v(e,n)},d(o){o&&c(e)}}}function oe(d){let e,n=`<!-- The button to open modal -->
+<label for="my-modal" class="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal" class="$$modal-toggle" />
+<div class="$$modal">
+  <div class="$$modal-box">
+    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="$$modal-action">
+      <label for="my-modal" class="$$btn">Yay!</label>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","html")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function ae(d){let e,n=`<!-- The button to open modal -->
+<label htmlFor="my-modal" className="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal" className="$$modal-toggle" />
+<div className="$$modal">
+  <div className="$$modal-box">
+    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="$$modal-action">
+      <label htmlFor="my-modal" className="$$btn">Yay!</label>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","react")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function se(d){let e,n;return{c(){e=f("a"),n=$("open modal"),this.h()},l(o){e=h(o,"A",{href:!0,class:!0,rel:!0});var l=b(e);n=_(l,"open modal"),l.forEach(c),this.h()},h(){u(e,"href","#my-modal-2"),u(e,"class","btn"),u(e,"rel","external")},m(o,l){p(o,e,l),v(e,n)},d(o){o&&c(e)}}}function ne(d){let e,n=`<!-- The button to open modal -->
+<a href="#my-modal-2" class="$$btn">open modal</a>
+<!-- Put this part before </body> tag -->
+<div class="$$modal" id="my-modal-2">
+  <div class="$$modal-box">
+    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="$$modal-action">
+     <a href="#" class="$$btn">Yay!</a>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","html")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function re(d){let e,n=`<!-- The button to open modal -->
+<a href="#my-modal-2" className="$$btn">open modal</a>
+<!-- Put this part before </body> tag -->
+<div className="$$modal" id="my-modal-2">
+  <div className="$$modal-box">
+    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="$$modal-action">
+     <a href="#" className="$$btn">Yay!</a>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","react")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function me(d){let e,n;return{c(){e=f("label"),n=$("open modal"),this.h()},l(o){e=h(o,"LABEL",{for:!0,class:!0});var l=b(e);n=_(l,"open modal"),l.forEach(c),this.h()},h(){u(e,"for","my-modal-3"),u(e,"class","btn modal-button")},m(o,l){p(o,e,l),v(e,n)},d(o){o&&c(e)}}}function de(d){let e,n=`<!-- The button to open modal -->
+<label for="my-modal-3" class="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-3" class="$$modal-toggle" />
+<div class="$$modal">
+  <div class="$$modal-box relative">
+    <label for="my-modal-3" class="$$btn $$btn-sm $$btn-circle absolute right-2 top-2">\u2715</label>
+    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","html")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function ce(d){let e,n=`<!-- The button to open modal -->
+<label htmlFor="my-modal-3" className="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-3" className="$$modal-toggle" />
+<div className="$$modal">
+  <div className="$$modal-box relative">
+    <label htmlFor="my-modal-3" className="$$btn $$btn-sm $$btn-circle absolute right-2 top-2">\u2715</label>
+    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","react")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function ie(d){let e,n;return{c(){e=f("label"),n=$("open modal"),this.h()},l(o){e=h(o,"LABEL",{for:!0,class:!0});var l=b(e);n=_(l,"open modal"),l.forEach(c),this.h()},h(){u(e,"for","my-modal-4"),u(e,"class","btn modal-button")},m(o,l){p(o,e,l),v(e,n)},d(o){o&&c(e)}}}function ue(d){let e,n=`<!-- The button to open modal -->
+<label for="my-modal-4" class="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-4" class="$$modal-toggle" />
+<label for="my-modal-4" class="$$modal cursor-pointer">
+  <label class="$$modal-box relative" for="">
+    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </label>
+</label>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","html")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function pe(d){let e,n=`<!-- The button to open modal -->
+<label htmlFor="my-modal-4" className="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-4" className="$$modal-toggle" />
+<label htmlFor="my-modal-4" className="$$modal cursor-pointer">
+  <label className="$$modal-box relative" for="">
+    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </label>
+</label>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","react")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function fe(d){let e,n;return{c(){e=f("label"),n=$("open modal"),this.h()},l(o){e=h(o,"LABEL",{for:!0,class:!0});var l=b(e);n=_(l,"open modal"),l.forEach(c),this.h()},h(){u(e,"for","my-modal-5"),u(e,"class","btn modal-button")},m(o,l){p(o,e,l),v(e,n)},d(o){o&&c(e)}}}function $e(d){let e,n=`<!-- The button to open modal -->
+<label for="my-modal-5" class="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-5" class="$$modal-toggle" />
+<div class="$$modal">
+  <div class="$$modal-box w-11/12 max-w-5xl">
+    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="$$modal-action">
+      <label for="my-modal-5" class="$$btn">Yay!</label>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","html")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function he(d){let e,n=`<!-- The button to open modal -->
+<label htmlFor="my-modal-5" className="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-5" className="$$modal-toggle" />
+<div className="$$modal">
+  <div className="$$modal-box w-11/12 max-w-5xl">
+    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="$$modal-action">
+      <label htmlFor="my-modal-5" className="$$btn">Yay!</label>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","react")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function be(d){let e,n;return{c(){e=f("label"),n=$("open modal"),this.h()},l(o){e=h(o,"LABEL",{for:!0,class:!0});var l=b(e);n=_(l,"open modal"),l.forEach(c),this.h()},h(){u(e,"for","my-modal-6"),u(e,"class","btn modal-button")},m(o,l){p(o,e,l),v(e,n)},d(o){o&&c(e)}}}function _e(d){let e,n=`<!-- The button to open modal -->
+<label for="my-modal-6" class="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-6" class="$$modal-toggle" />
+<div class="$$modal $$modal-bottom sm:$$modal-middle">
+  <div class="$$modal-box">
+    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="$$modal-action">
+      <label for="my-modal-6" class="$$btn">Yay!</label>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","html")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function ve(d){let e,n=`<!-- The button to open modal -->
+<label htmlFor="my-modal-6" className="$$btn $$modal-button">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-6" className="$$modal-toggle" />
+<div className="$$modal $$modal-bottom sm:$$modal-middle">
+  <div className="$$modal-box">
+    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="$$modal-action">
+      <label htmlFor="my-modal-6" className="$$btn">Yay!</label>
+    </div>
+  </div>
+</div>`,o,l,s,m;return{c(){e=f("pre"),o=$(n),this.h()},l(t){e=h(t,"PRE",{slot:!0});var a=b(e);o=_(a,n),a.forEach(c),this.h()},h(){u(e,"slot","react")},m(t,a){p(t,e,a),v(e,o),s||(m=g(l=x.call(null,e,{to:d[0]})),s=!0)},p(t,a){l&&y(l.update)&&a&1&&l.update.call(null,{to:t[0]})},d(t){t&&c(e),s=!1,m()}}}function ge(d){let e,n,o,l,s,m,t,a,N,R,k,A,E,L;return e=new te({props:{data:[{type:"component",class:"modal",desc:"Container element"},{type:"component",class:"modal-box",desc:"The content of modal"},{type:"component",class:"modal-action",desc:"Container for modal buttons"},{type:"component",class:"modal-toggle",desc:"For checkbox that controls modal"},{type:"component",class:"modal-button",desc:"For <label> that checks the checkbox to opens/closes modal"},{type:"modifier",class:"modal-open",desc:"Add/remove this class to open/close the modal using JS"},{type:"responsive",class:"modal-bottom",desc:"Moves the modal to bottom"},{type:"responsive",class:"modal-middle",desc:"Moves the modal to middle (default)"}]}}),o=new F({props:{title:"Modal using label + hidden checkbox",desc:'Make sure each modal you use, has a unique ID. In this example, ID is "my-modal".',$$slots:{react:[ae],html:[oe],default:[le]},$$scope:{ctx:d}}}),s=new F({props:{title:"Modal using anchor link",desc:"Anchor links might not work well on some SPA frameworks so if there are problems, use the first example",$$slots:{react:[re],html:[ne],default:[se]},$$scope:{ctx:d}}}),t=new F({props:{title:"Modal using with a close button at corner",$$slots:{react:[ce],html:[de],default:[me]},$$scope:{ctx:d}}}),N=new F({props:{title:"Modal that closes when clicked outside",desc:"Modal works with a hidden checkbox and labels can toggle the checkbox so we can use a label tag for the whole modal and use another label for modal-box to prevent closing when modal-box is clicked",$$slots:{react:[pe],html:[ue],default:[ie]},$$scope:{ctx:d}}}),k=new F({props:{title:"Modal with custom width",desc:"We remove max-width so we can use a custom size",$$slots:{react:[he],html:[$e],default:[fe]},$$scope:{ctx:d}}}),E=new F({props:{title:"Responsive: Modal goes bottom on mobile screen and goes middle on desktop",$$slots:{react:[ve],html:[_e],default:[be]},$$scope:{ctx:d}}}),{c(){w(e.$$.fragment),n=T(),w(o.$$.fragment),l=T(),w(s.$$.fragment),m=T(),w(t.$$.fragment),a=T(),w(N.$$.fragment),R=T(),w(k.$$.fragment),A=T(),w(E.$$.fragment)},l(r){P(e.$$.fragment,r),n=W(r),P(o.$$.fragment,r),l=W(r),P(s.$$.fragment,r),m=W(r),P(t.$$.fragment,r),a=W(r),P(N.$$.fragment,r),R=W(r),P(k.$$.fragment,r),A=W(r),P(E.$$.fragment,r)},m(r,i){Y(e,r,i),p(r,n,i),Y(o,r,i),p(r,l,i),Y(s,r,i),p(r,m,i),Y(t,r,i),p(r,a,i),Y(N,r,i),p(r,R,i),Y(k,r,i),p(r,A,i),Y(E,r,i),L=!0},p(r,i){const S={};i&5&&(S.$$scope={dirty:i,ctx:r}),o.$set(S);const q={};i&5&&(q.$$scope={dirty:i,ctx:r}),s.$set(q);const z={};i&5&&(z.$$scope={dirty:i,ctx:r}),t.$set(z);const D={};i&5&&(D.$$scope={dirty:i,ctx:r}),N.$set(D);const j={};i&5&&(j.$$scope={dirty:i,ctx:r}),k.$set(j);const J={};i&5&&(J.$$scope={dirty:i,ctx:r}),E.$set(J)},i(r){L||(C(e.$$.fragment,r),C(o.$$.fragment,r),C(s.$$.fragment,r),C(t.$$.fragment,r),C(N.$$.fragment,r),C(k.$$.fragment,r),C(E.$$.fragment,r),L=!0)},o(r){I(e.$$.fragment,r),I(o.$$.fragment,r),I(s.$$.fragment,r),I(t.$$.fragment,r),I(N.$$.fragment,r),I(k.$$.fragment,r),I(E.$$.fragment,r),L=!1},d(r){M(e,r),r&&c(n),M(o,r),r&&c(l),M(s,r),r&&c(m),M(t,r),r&&c(a),M(N,r),r&&c(R),M(k,r),r&&c(A),M(E,r)}}}function ye(d){let e,n;const o=[d[1],H];let l={$$slots:{default:[ge]},$$scope:{ctx:d}};for(let s=0;s<o.length;s+=1)l=B(l,o[s]);return e=new Z({props:l}),{c(){w(e.$$.fragment)},l(s){P(e.$$.fragment,s)},m(s,m){Y(e,s,m),n=!0},p(s,[m]){const t=m&2?V(o,[m&2&&K(s[1]),m&0&&K(H)]):{};m&5&&(t.$$scope={dirty:m,ctx:s}),e.$set(t)},i(s){n||(C(e.$$.fragment,s),n=!0)},o(s){I(e.$$.fragment,s),n=!1},d(s){M(e,s)}}}const H={title:"Modal",desc:"Modal is used to show a dialog or a box when you click a button.",published:!0};function xe(d,e,n){let o;return X(d,ee,l=>n(0,o=l)),d.$$set=l=>{n(1,e=B(B({},e),G(l)))},e=G(e),[o,e]}class Te extends O{constructor(e){super();Q(this,e,xe,ye,U,{})}}export{Te as default,H as metadata};
