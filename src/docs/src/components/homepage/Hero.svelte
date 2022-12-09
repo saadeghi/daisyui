7ec7d3b9d60d3d18fbf7b1d9b402ee0b29924510
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-12-09 18:36:41 +0300

    docs: update homepage content

--- src/docs/src/components/homepage/Hero.svelte
+++ src/docs/src/components/homepage/Hero.svelte
@@ -7,8 +7,8 @@
   <div class="hero-content col-start-1 row-start-1 w-full max-w-7xl flex-col justify-between gap-10 pb-40 lg:flex-row lg:items-end lg:gap-0 xl:gap-20">
     <div class="lg:pl-10 lg:pb-32">
       <div class="mb-2 py-4 text-center lg:py-10 lg:text-left">
-        <h1 class="font-title mb-2 text-4xl font-extrabold sm:text-5xl lg:text-7xl">daisyUI</h1>
-        <h2 class="font-title text-lg font-extrabold sm:text-2xl lg:text-3xl">
+        <h1 class="font-title mb-2 text-4xl font-extrabold sm:text-5xl lg:text-6xl">{@html $t("daisyUI components")}</h1>
+        <h2 class="font-title text-lg font-extrabold sm:text-xl lg:text-2xl">
           {@html $t("daisyui-tagline")}
         </h2>
       </div>
