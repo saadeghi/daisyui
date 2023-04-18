822b38e2be8afcfc225b92471000899a930fba95
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-04-19 01:27:23 +0300

    improve playground DX

--- src/experiments/playground/tailwind.config.js
+++ src/experiments/playground/tailwind.config.js
@@ -1,19 +1,22 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin")
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
   daisyui: {
-    themes: [
-      'light'
-    ],
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
   },
 
-
   plugins: [
-    require('@tailwindcss/typography'),
-    require('../../'),
+    require("@tailwindcss/typography"),
+    require("../../"),
     // require('daisyui'),
     // require('tailwindcss-flip'),
-
   ],
 }
