28787afabe65e835bda46ae64d1fe17015322157
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-05-25 16:17:44 +0300

    docs: fix class name issue

--- src/docs/src/routes/+page.svelte
+++ src/docs/src/routes/+page.svelte
@@ -1317,7 +1317,7 @@
                 ? 0.1
                 : Math.trunc(animateValue(section[7], [-100, -20], [0, 1]))
             }`}
-            class={`[&::selection]:text-base-content relative col-start-1 row-start-1 leading-tight [&::selection]:bg-blue-700/20${
+            class={`[&::selection]:text-base-content relative col-start-1 row-start-1 leading-tight [&::selection]:bg-blue-700/20 ${
               Math.trunc(animateValue(section[7], [-100, -20], [0, 1])) !== 0
                 ? "bg-[linear-gradient(90deg,hsl(var(--s))_0%,hsl(var(--sf))_9%,hsl(var(--pf))_42%,hsl(var(--p))_47%,hsl(var(--a))_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,hsl(var(--s))_4%,color-mix(in_oklch,hsl(var(--sf)),hsl(var(--pf)))_22%,hsl(var(--p))_45%,color-mix(in_oklch,hsl(var(--p)),hsl(var(--a)))_67%,hsl(var(--a))_100.2%)]"
                 : ""
