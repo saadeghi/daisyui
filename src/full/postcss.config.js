0bf1663393d3cff58089ffe9f2c1255e0378d891
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-06-07 12:51:40 +0300

    fix: #1916

--- src/full/postcss.config.js
+++ src/full/postcss.config.js
@@ -3,5 +3,6 @@ module.exports = {
     require("postcss-import"),
     require("tailwindcss/nesting"),
     require("tailwindcss")("./src/full/tailwind.config.js"),
+    require("autoprefixer"),
   ],
 }
