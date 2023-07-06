b4620e7103be247acae68da2e193c8e603adf906
saadeghi <saadeghi@users.noreply.github.com>
2023-07-06 22:36:08 +0000

    deploy: d830212eac603b8c858954f0cf876293b983e1aa

--- _app/immutable/chunks/stores.91305f73.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./singletons.070ffc76.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
