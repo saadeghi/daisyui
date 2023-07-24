0cc751cad37f32f21b4ce9929e7d7caf7ff5e665
saadeghi <saadeghi@users.noreply.github.com>
2023-07-24 10:32:19 +0000

    deploy: 53c079eefe156baf72457a6f5aaab5bf4e95e382

---
+++ _app/immutable/nodes/11.d23c4e54.js
@@ -0,0 +1,37 @@
+import{s as S,E as L,y as O,O as P,a as b,c as k,i as p,d as m,f as d,g as h,z as M,j as f,w as B,l as D,h as V,m as j,v as C,Y as z,U as E}from"../chunks/scheduler.e5ccafd2.js";import{S as U,i as Y,b as _,d as $,m as v,a as g,t as w,e as x}from"../chunks/index.fb4b3a7b.js";import{g as F,a as R}from"../chunks/index.18b79e66.js";import{M as G,p as J,C as K,a as H,r as T}from"../chunks/ClassTable.cd9ad35c.js";function N(u){let t,o="<ul><li><a>Home</a></li> <li><a>Documents</a></li> <li>Add Document</li></ul>";return{c(){t=d("div"),t.innerHTML=o,this.h()},l(e){t=h(e,"DIV",{class:!0,"data-svelte-h":!0}),M(t)!=="svelte-1ky13dk"&&(t.innerHTML=o),this.h()},h(){f(t,"class","text-sm breadcrumbs")},m(e,n){p(e,t,n)},p:B,d(e){e&&m(t)}}}function Q(u){let t,o=`<div class="text-sm $$breadcrumbs">
+  <ul>
+    <li><a>Home</a></li> 
+    <li><a>Documents</a></li> 
+    <li>Add Document</li>
+  </ul>
+</div>`,e,n,s,i;return{c(){t=d("pre"),e=D(o),this.h()},l(l){t=h(l,"PRE",{slot:!0});var r=V(t);e=j(r,o),r.forEach(m),this.h()},h(){f(t,"slot","html")},m(l,r){p(l,t,r),C(t,e),s||(i=z(n=T.call(null,t,{to:u[0]})),s=!0)},p(l,r){n&&E(n.update)&&r&1&&n.update.call(null,{to:l[0]})},d(l){l&&m(t),s=!1,i()}}}function W(u){let t,o=`<ul><li><a><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+        Home</a></li> <li><a><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+        Documents</a></li> <li><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+      Add Document</li></ul>`;return{c(){t=d("div"),t.innerHTML=o,this.h()},l(e){t=h(e,"DIV",{class:!0,"data-svelte-h":!0}),M(t)!=="svelte-1nk7pnq"&&(t.innerHTML=o),this.h()},h(){f(t,"class","text-sm breadcrumbs")},m(e,n){p(e,t,n)},p:B,d(e){e&&m(t)}}}function X(u){let t,o=`<div class="text-sm $$breadcrumbs">
+  <ul>
+    <li>
+      <a>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+        Home
+      </a>
+    </li> 
+    <li>
+      <a>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+        Documents
+      </a>
+    </li> 
+    <li>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-2 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+      Add Document
+    </li>
+  </ul>
+</div>`,e,n,s,i;return{c(){t=d("pre"),e=D(o),this.h()},l(l){t=h(l,"PRE",{slot:!0});var r=V(t);e=j(r,o),r.forEach(m),this.h()},h(){f(t,"slot","html")},m(l,r){p(l,t,r),C(t,e),s||(i=z(n=T.call(null,t,{to:u[0]})),s=!0)},p(l,r){n&&E(n.update)&&r&1&&n.update.call(null,{to:l[0]})},d(l){l&&m(t),s=!1,i()}}}function Z(u){let t,o="<ul><li>Long text 1</li> <li>Long text 2</li> <li>Long text 3</li> <li>Long text 4</li> <li>Long text 5</li></ul>";return{c(){t=d("div"),t.innerHTML=o,this.h()},l(e){t=h(e,"DIV",{class:!0,"data-svelte-h":!0}),M(t)!=="svelte-10d6jh1"&&(t.innerHTML=o),this.h()},h(){f(t,"class","max-w-xs text-sm breadcrumbs")},m(e,n){p(e,t,n)},p:B,d(e){e&&m(t)}}}function tt(u){let t,o=`<div class="max-w-xs text-sm $$breadcrumbs">
+  <ul>
+    <li>Long text 1</li>
+    <li>Long text 2</li>
+    <li>Long text 3</li>
+    <li>Long text 4</li>
+    <li>Long text 5</li>
+  </ul>
+</div>`,e,n,s,i;return{c(){t=d("pre"),e=D(o),this.h()},l(l){t=h(l,"PRE",{slot:!0});var r=V(t);e=j(r,o),r.forEach(m),this.h()},h(){f(t,"slot","html")},m(l,r){p(l,t,r),C(t,e),s||(i=z(n=T.call(null,t,{to:u[0]})),s=!0)},p(l,r){n&&E(n.update)&&r&1&&n.update.call(null,{to:l[0]})},d(l){l&&m(t),s=!1,i()}}}function et(u){let t,o,e,n,s,i,l,r;return t=new K({props:{data:[{type:"component",class:"breadcrumbs",desc:"Container element"}]}}),e=new H({props:{title:"Breadcrumbs",$$slots:{html:[Q],default:[N]},$$scope:{ctx:u}}}),s=new H({props:{title:"Breadcrumbs with icons",$$slots:{html:[X],default:[W]},$$scope:{ctx:u}}}),l=new H({props:{title:"Breadcrumbs with max-width",desc:"If you set max-width or the list gets larger than the container it will scroll",$$slots:{html:[tt],default:[Z]},$$scope:{ctx:u}}}),{c(){_(t.$$.fragment),o=b(),_(e.$$.fragment),n=b(),_(s.$$.fragment),i=b(),_(l.$$.fragment)},l(a){$(t.$$.fragment,a),o=k(a),$(e.$$.fragment,a),n=k(a),$(s.$$.fragment,a),i=k(a),$(l.$$.fragment,a)},m(a,c){v(t,a,c),p(a,o,c),v(e,a,c),p(a,n,c),v(s,a,c),p(a,i,c),v(l,a,c),r=!0},p(a,c){const y={};c&5&&(y.$$scope={dirty:c,ctx:a}),e.$set(y);const A={};c&5&&(A.$$scope={dirty:c,ctx:a}),s.$set(A);const I={};c&5&&(I.$$scope={dirty:c,ctx:a}),l.$set(I)},i(a){r||(g(t.$$.fragment,a),g(e.$$.fragment,a),g(s.$$.fragment,a),g(l.$$.fragment,a),r=!0)},o(a){w(t.$$.fragment,a),w(e.$$.fragment,a),w(s.$$.fragment,a),w(l.$$.fragment,a),r=!1},d(a){a&&(m(o),m(n),m(i)),x(t,a),x(e,a),x(s,a),x(l,a)}}}function st(u){let t,o;const e=[u[1],q];let n={$$slots:{default:[et]},$$scope:{ctx:u}};for(let s=0;s<e.length;s+=1)n=L(n,e[s]);return t=new G({props:n}),{c(){_(t.$$.fragment)},l(s){$(t.$$.fragment,s)},m(s,i){v(t,s,i),o=!0},p(s,[i]){const l=i&2?F(e,[i&2&&R(s[1]),i&0&&R(q)]):{};i&5&&(l.$$scope={dirty:i,ctx:s}),t.$set(l)},i(s){o||(g(t.$$.fragment,s),o=!0)},o(s){w(t.$$.fragment,s),o=!1},d(s){x(t,s)}}}const q={title:"Breadcrumbs",desc:"Breadcrumbs helps users to navigate through the website.",published:!0,layout:"components"};function lt(u,t,o){let e;return O(u,J,n=>o(0,e=n)),u.$$set=n=>{o(1,t=L(L({},t),P(n)))},t=P(t),[e,t]}class it extends U{constructor(t){super(),Y(this,t,lt,st,S,{})}}export{it as component};
