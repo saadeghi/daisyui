8a0c5003d7cd20403bce26c3ac72e73bc51ad4ec
Tech Genius <supratikmahato7777@gmail.com>
2023-08-30 11:59:31 +0530

    style: fixed styling inconsistencies

--- src/theming/themes.d.ts
+++ src/theming/themes.d.ts
@@ -1,5 +1,5 @@
-import { CustomTheme, Theme } from '../index';
+import { CustomTheme, Theme } from "../index"
 
-declare const themes: Record<`[data-theme=${Theme}]`, CustomTheme[string]>;
+declare const themes: Record<`[data-theme=${Theme}]`, CustomTheme[string]>
 
-export default themes;
+export default themes
