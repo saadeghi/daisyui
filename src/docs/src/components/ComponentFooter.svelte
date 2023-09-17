9e613e9d34fedfd48161879ac23f6358f2d2c2f7
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-09-17 11:07:59 +0300

    docs

--- src/docs/src/components/ComponentFooter.svelte
+++ src/docs/src/components/ComponentFooter.svelte
@@ -1,6 +1,5 @@
 <script>
   import { page } from "$app/stores"
-  import { default as emojiUnicode } from "emoji-unicode"
   import { pages } from "@src/lib/data.js"
 
   import { currentLang, defaultLang, t } from "@src/lib/i18n"
@@ -181,14 +180,6 @@
         </div>
         {#if $currentLang != defaultLang}
           <div class="flex items-center gap-2">
-            <img
-              loading="lazy"
-              width="20"
-              height="20"
-              alt={$t("__name", {}, $currentLang)}
-              src={`https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.0/svg/${emojiUnicode(
-                $t("__flag", {}, $currentLang)
-              ).replace(/\s/g, "-")}.svg`} />
             <div>
               <a
                 target="_blank"
