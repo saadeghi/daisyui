3e67efb59161fddef213600897de8bed0f91fe47
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-09-28 18:04:54 +0300

    fix RSS

--- src/docs/src/routes/blog/rss.xml/+server.js
+++ src/docs/src/routes/blog/rss.xml/+server.js
@@ -48,7 +48,6 @@ export const GET = async () => {
 const render = (
   postsInfo
 ) => `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
-${postsInfo}
   <channel>
     <title>
       <![CDATA[ ${siteTitle} ]]>
