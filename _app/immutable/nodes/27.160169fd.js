c3d33f56635bc172c3fe01c143ab8b3e357b9a8d
saadeghi <saadeghi@users.noreply.github.com>
2023-09-30 03:46:10 +0000

    deploy: 4d6464d6457cb971f656d8ab4a8ab535f9db0b54

---
+++ _app/immutable/nodes/27.160169fd.js
@@ -0,0 +1,74 @@
+import{s as X,N as R,z as J,S as F,a as C,c as L,i as p,d as u,f as m,g as f,A as T,j as h,y as M,l as q,h as H,m as P,x as k,X as I,E as S,k as K}from"../chunks/scheduler.5afb8bf4.js";import{S as U,i as W,b,d as x,m as g,a as _,t as w,e as y}from"../chunks/index.2536167e.js";import{g as Y,a as z}from"../chunks/spread.8a54911c.js";import{M as Z,p as ee,C as te,a as j,r as E}from"../chunks/ClassTable.a1fff651.js";function ae(c){let e,o='<div class="text-center hero-content"><div class="max-w-md"><h3 class="text-5xl font-bold">Hello there</h3> <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> <button class="btn btn-primary">Get Started</button></div></div>';return{c(){e=m("div"),e.innerHTML=o,this.h()},l(t){e=f(t,"DIV",{class:!0,"data-svelte-h":!0}),T(e)!=="svelte-b1oxtm"&&(e.innerHTML=o),this.h()},h(){h(e,"class","hero min-h-[30rem] rounded bg-base-200")},m(t,s){p(t,e,s)},p:M,d(t){t&&u(e)}}}function se(c){let e,o=`<div class="$$hero min-h-screen bg-base-200">
+  <div class="$$hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello there</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="$$btn $$btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>`,t,s,n,r;return{c(){e=m("pre"),t=q(o),this.h()},l(a){e=f(a,"PRE",{slot:!0});var i=H(e);t=P(i,o),i.forEach(u),this.h()},h(){h(e,"slot","html")},m(a,i){p(a,e,i),k(e,t),n||(r=I(s=E.call(null,e,{to:c[0]})),n=!0)},p(a,i){s&&S(s.update)&&i&1&&s.update.call(null,{to:a[0]})},d(a){a&&u(e),n=!1,r()}}}function le(c){let e,o='<div class="flex-col hero-content lg:flex-row"><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" alt="Tailwind CSS hero component"/> <div><h3 class="text-5xl font-bold">Box Office News!</h3> <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> <button class="btn btn-primary">Get Started</button></div></div>';return{c(){e=m("div"),e.innerHTML=o,this.h()},l(t){e=f(t,"DIV",{class:!0,"data-svelte-h":!0}),T(e)!=="svelte-ndrrj3"&&(e.innerHTML=o),this.h()},h(){h(e,"class","hero min-h-[30rem] rounded bg-base-200")},m(t,s){p(t,e,s)},p:M,d(t){t&&u(e)}}}function ne(c){let e,o=`<div class="$$hero min-h-screen bg-base-200">
+  <div class="$$hero-content flex-col lg:flex-row">
+    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="$$btn $$btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>`,t,s,n,r;return{c(){e=m("pre"),t=q(o),this.h()},l(a){e=f(a,"PRE",{slot:!0});var i=H(e);t=P(i,o),i.forEach(u),this.h()},h(){h(e,"slot","html")},m(a,i){p(a,e,i),k(e,t),n||(r=I(s=E.call(null,e,{to:c[0]})),n=!0)},p(a,i){s&&S(s.update)&&i&1&&s.update.call(null,{to:a[0]})},d(a){a&&u(e),n=!1,r()}}}function ie(c){let e,o='<div class="flex-col hero-content lg:flex-row-reverse"><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" alt="Tailwind CSS hero component"/> <div><h3 class="text-5xl font-bold">Box Office News!</h3> <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> <button class="btn btn-primary">Get Started</button></div></div>';return{c(){e=m("div"),e.innerHTML=o,this.h()},l(t){e=f(t,"DIV",{class:!0,"data-svelte-h":!0}),T(e)!=="svelte-1vawo1k"&&(e.innerHTML=o),this.h()},h(){h(e,"class","hero min-h-[30rem] rounded bg-base-200")},m(t,s){p(t,e,s)},p:M,d(t){t&&u(e)}}}function oe(c){let e,o=`<div class="$$hero min-h-screen bg-base-200">
+  <div class="$$hero-content flex-col lg:flex-row-reverse">
+    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="$$btn $$btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>`,t,s,n,r;return{c(){e=m("pre"),t=q(o),this.h()},l(a){e=f(a,"PRE",{slot:!0});var i=H(e);t=P(i,o),i.forEach(u),this.h()},h(){h(e,"slot","html")},m(a,i){p(a,e,i),k(e,t),n||(r=I(s=E.call(null,e,{to:c[0]})),n=!0)},p(a,i){s&&S(s.update)&&i&1&&s.update.call(null,{to:a[0]})},d(a){a&&u(e),n=!1,r()}}}function re(c){let e,o='<div class="flex-col hero-content lg:flex-row-reverse"><div class="text-center lg:text-left"><h3 class="text-5xl font-bold">Login now!</h3> <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p></div> <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"><div class="card-body"><div class="form-control"><label class="label"><span class="label-text">Email</span></label> <input type="text" placeholder="email" class="input input-bordered"/></div> <div class="form-control"><label class="label"><span class="label-text">Password</span></label> <input type="text" placeholder="password" class="input input-bordered"/> <label class="label"><a href="#" class="label-text-alt link link-hover">Forgot password?</a></label></div> <div class="form-control mt-6"><button class="btn btn-primary">Login</button></div></div></div></div>';return{c(){e=m("div"),e.innerHTML=o,this.h()},l(t){e=f(t,"DIV",{class:!0,"data-svelte-h":!0}),T(e)!=="svelte-dpg0ry"&&(e.innerHTML=o),this.h()},h(){h(e,"class","hero min-h-[30rem] rounded bg-base-200")},m(t,s){p(t,e,s)},p:M,d(t){t&&u(e)}}}function ce(c){let e,o=`<div class="$$hero min-h-screen bg-base-200">
+  <div class="$$hero-content flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Login now!</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+    </div>
+    <div class="$$card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="$$card-body">
+        <div class="$$form-control">
+          <label class="$$label">
+            <span class="$$label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" class="$$input $$input-bordered" />
+        </div>
+        <div class="$$form-control">
+          <label class="$$label">
+            <span class="$$label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" class="$$input $$input-bordered" />
+          <label class="$$label">
+            <a href="#" class="$$label-text-alt $$link $$link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div class="$$form-control mt-6">
+          <button class="$$btn $$btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`,t,s,n,r;return{c(){e=m("pre"),t=q(o),this.h()},l(a){e=f(a,"PRE",{slot:!0});var i=H(e);t=P(i,o),i.forEach(u),this.h()},h(){h(e,"slot","html")},m(a,i){p(a,e,i),k(e,t),n||(r=I(s=E.call(null,e,{to:c[0]})),n=!0)},p(a,i){s&&S(s.update)&&i&1&&s.update.call(null,{to:a[0]})},d(a){a&&u(e),n=!1,r()}}}function de(c){let e,o='<div class="hero-overlay rounded bg-opacity-60"></div> <div class="text-center hero-content text-neutral-content"><div class="max-w-md"><h1 class="mb-5 text-5xl font-bold">Hello there</h1> <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> <button class="btn btn-primary">Get Started</button></div></div>';return{c(){e=m("div"),e.innerHTML=o,this.h()},l(t){e=f(t,"DIV",{class:!0,style:!0,"data-svelte-h":!0}),T(e)!=="svelte-1qb4t8e"&&(e.innerHTML=o),this.h()},h(){h(e,"class","hero min-h-[30rem] rounded"),K(e,"background-image","url(/images/stock/photo-1507358522600-9f71e620c44e.jpg)")},m(t,s){p(t,e,s)},p:M,d(t){t&&u(e)}}}function ue(c){let e,o=`<div class="$$hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+  <div class="$$hero-overlay bg-opacity-60"></div>
+  <div class="$$hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="$$btn $$btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>`,t,s,n,r;return{c(){e=m("pre"),t=q(o),this.h()},l(a){e=f(a,"PRE",{slot:!0});var i=H(e);t=P(i,o),i.forEach(u),this.h()},h(){h(e,"slot","html")},m(a,i){p(a,e,i),k(e,t),n||(r=I(s=E.call(null,e,{to:c[0]})),n=!0)},p(a,i){s&&S(s.update)&&i&1&&s.update.call(null,{to:a[0]})},d(a){a&&u(e),n=!1,r()}}}function pe(c){let e,o=`<div class="$$hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+  <div class="$$hero-overlay bg-opacity-60"></div>
+  <div class="$$hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="$$btn $$btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>`,t,s,n,r;return{c(){e=m("pre"),t=q(o),this.h()},l(a){e=f(a,"PRE",{slot:!0});var i=H(e);t=P(i,o),i.forEach(u),this.h()},h(){h(e,"slot","jsx")},m(a,i){p(a,e,i),k(e,t),n||(r=I(s=E.call(null,e,{to:c[0]})),n=!0)},p(a,i){s&&S(s.update)&&i&1&&s.update.call(null,{to:a[0]})},d(a){a&&u(e),n=!1,r()}}}function me(c){let e,o,t,s,n,r,a,i,$,Q,v,G;return e=new te({props:{data:[{type:"component",class:"hero",desc:"Container element"},{type:"component",class:"hero-content",desc:"Container for content"},{type:"component",class:"hero-overlay",desc:"Overlay that covers the background image"}]}}),t=new j({props:{title:"Centered hero",$$slots:{html:[se],default:[ae]},$$scope:{ctx:c}}}),n=new j({props:{title:"Hero with figure",$$slots:{html:[ne],default:[le]},$$scope:{ctx:c}}}),a=new j({props:{title:"Hero with figure but reverse order",$$slots:{html:[oe],default:[ie]},$$scope:{ctx:c}}}),$=new j({props:{title:"Hero with form",$$slots:{html:[ce],default:[re]},$$scope:{ctx:c}}}),v=new j({props:{title:"Hero with overlay image",$$slots:{jsx:[pe],html:[ue],default:[de]},$$scope:{ctx:c}}}),{c(){b(e.$$.fragment),o=C(),b(t.$$.fragment),s=C(),b(n.$$.fragment),r=C(),b(a.$$.fragment),i=C(),b($.$$.fragment),Q=C(),b(v.$$.fragment)},l(l){x(e.$$.fragment,l),o=L(l),x(t.$$.fragment,l),s=L(l),x(n.$$.fragment,l),r=L(l),x(a.$$.fragment,l),i=L(l),x($.$$.fragment,l),Q=L(l),x(v.$$.fragment,l)},m(l,d){g(e,l,d),p(l,o,d),g(t,l,d),p(l,s,d),g(n,l,d),p(l,r,d),g(a,l,d),p(l,i,d),g($,l,d),p(l,Q,d),g(v,l,d),G=!0},p(l,d){const D={};d&5&&(D.$$scope={dirty:d,ctx:l}),t.$set(D);const N={};d&5&&(N.$$scope={dirty:d,ctx:l}),n.$set(N);const O={};d&5&&(O.$$scope={dirty:d,ctx:l}),a.$set(O);const V={};d&5&&(V.$$scope={dirty:d,ctx:l}),$.$set(V);const B={};d&5&&(B.$$scope={dirty:d,ctx:l}),v.$set(B)},i(l){G||(_(e.$$.fragment,l),_(t.$$.fragment,l),_(n.$$.fragment,l),_(a.$$.fragment,l),_($.$$.fragment,l),_(v.$$.fragment,l),G=!0)},o(l){w(e.$$.fragment,l),w(t.$$.fragment,l),w(n.$$.fragment,l),w(a.$$.fragment,l),w($.$$.fragment,l),w(v.$$.fragment,l),G=!1},d(l){l&&(u(o),u(s),u(r),u(i),u(Q)),y(e,l),y(t,l),y(n,l),y(a,l),y($,l),y(v,l)}}}function fe(c){let e,o;const t=[c[1],A];let s={$$slots:{default:[me]},$$scope:{ctx:c}};for(let n=0;n<t.length;n+=1)s=R(s,t[n]);return e=new Z({props:s}),{c(){b(e.$$.fragment)},l(n){x(e.$$.fragment,n)},m(n,r){g(e,n,r),o=!0},p(n,[r]){const a=r&2?Y(t,[r&2&&z(n[1]),r&0&&z(A)]):{};r&5&&(a.$$scope={dirty:r,ctx:n}),e.$set(a)},i(n){o||(_(e.$$.fragment,n),o=!0)},o(n){w(e.$$.fragment,n),o=!1},d(n){y(e,n)}}}const A={title:"Hero",desc:"Hero is a component for displaying a large box or image with a title and description.",published:!0,layout:"components"};function he(c,e,o){let t;return J(c,ee,s=>o(0,t=s)),c.$$set=s=>{o(1,e=R(R({},e),F(s)))},e=F(e),[t,e]}class ge extends U{constructor(e){super(),W(this,e,he,fe,X,{})}}export{ge as component};
