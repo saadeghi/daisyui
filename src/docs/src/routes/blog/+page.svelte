2051983aa368454b39193f03aeb5fcac6f085a43
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-10-05 22:53:52 +0300

    update thumbnails

--- src/docs/src/routes/blog/+page.svelte
+++ src/docs/src/routes/blog/+page.svelte
@@ -4,7 +4,10 @@
   let { posts } = data
 </script>
 
-<SEO title="daisyUI Blog" desc="daisyUI blog - Updates, ideas and resources" />
+<SEO
+  title="daisyUI Blog"
+  desc="daisyUI blog - Updates, ideas and resources"
+  img="/images/blog.jpg" />
 {#if posts.length === 0}
   <div
     class="border-base-content/10 rounded-box flex h-96 w-full grow flex-col items-center justify-center border">
