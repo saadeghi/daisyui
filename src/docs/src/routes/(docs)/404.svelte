260b2ba9f48e50f9946e14675928978971707301
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-10-12 09:53:04 +0300

    add 404 page

--- src/docs/src/routes/(docs)/404.svelte
+++
@@ -1,9 +0,0 @@
-<div class="hero not-prose min-h-screen">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold opacity-10 lg:text-7xl xl:text-9xl">404</h1>
-      <p class="mb-5">Page not found</p>
-      <a class="btn" href="/">Go back</a>
-    </div>
-  </div>
-</div>
---
+++ src/docs/src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from "$app/stores"
+  import "@components/StyleHandler.svelte"
+</script>
+
+<div class="hero not-prose min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold opacity-10 lg:text-7xl xl:text-9xl">Error</h1>
+      <p class="mb-5">{$page.error.message}</p>
+      <a class="btn" href="/">Go back</a>
+    </div>
+  </div>
+</div>
