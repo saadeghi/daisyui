1b4121627b4858c9d769050e1bef76a62086ddd0
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-12 16:13:56 +0330

    update tailwindplay ling

--- src/docs/src/routes/(redirects)/tailwindplay/+page.svelte
+++ src/docs/src/routes/(redirects)/tailwindplay/+page.svelte
@@ -2,7 +2,7 @@
   export async function load() {
     return {
       status: 302,
-      redirect: "https://play.tailwindcss.com/eUmbFJcv1M",
+      redirect: "https://play.tailwindcss.com/IIWW8U8Zeq",
     }
   }
 </script>
@@ -10,6 +10,6 @@
 <script>
   import { onMount } from "svelte"
   onMount(() => {
-    window.location.href = "https://play.tailwindcss.com/eUmbFJcv1M"
+    window.location.href = "https://play.tailwindcss.com/IIWW8U8Zeq"
   })
 </script>
