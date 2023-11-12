c8623e24f0813542883c8c5fe4609341c0550d3d
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-12 15:59:16 +0330

    playground

--- src/experiments/playground/tailwind.config.js
+++ src/experiments/playground/tailwind.config.js
@@ -3,14 +3,13 @@ const plugin = require("tailwindcss/plugin")
 module.exports = {
   content: ["./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}"],
   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
+    // styled: 0,
+    // themes: 0,
+    // base: 0,
+    // utils: 0,
+    // logs: true,
+    // prefix: "b-",
+    // darkTheme: "dark",
   },
 
   plugins: [
