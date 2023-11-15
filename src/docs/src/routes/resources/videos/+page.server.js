db2a2a7d0ce8f3c899c0f12f23d6e05c21f8cc3f
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-16 02:58:41 +0330

    docs: improve videos page

--- src/docs/src/routes/resources/videos/+page.server.js
+++ src/docs/src/routes/resources/videos/+page.server.js
@@ -1,7 +1,10 @@
 import videos from "$lib/json/youtube.json"
-
+// filter unused properties
+const videosFiltered = videos.map(
+  ({ statistics, pageInfo, kind, etag, contentDetails, ...rest }) => rest
+)
 export async function load() {
   return {
-    videos,
+    videos: videosFiltered,
   }
 }
