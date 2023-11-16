ad099e342f5bd50b1ab223280cdcc0e5cfedb872
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-16 16:29:55 +0330

    scripts

---
+++ src/release.js
@@ -0,0 +1,15 @@
+import { execSync } from "child_process"
+
+// available flags:
+// npm run release -- --alpha
+// npm run release -- --patch
+// npm run release -- --minor
+// npm run release -- --major
+
+execSync(
+  `node_modules/.bin/commit-and-tag-version${
+    process.argv.includes("--alpha") ? " --prerelease alpha" : ""
+  }${process.argv.includes("--patch") ? " --release-as patch" : ""}${
+    process.argv.includes("--minor") ? " --release-as minor" : ""
+  }${process.argv.includes("--major") ? " --release-as major" : ""}`
+)
