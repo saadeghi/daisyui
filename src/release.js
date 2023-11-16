d2c1264d985a3a3ec8c1a95ff33ae743b39a6770
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-16 16:46:36 +0330

    scripts

--- src/release.js
+++ src/release.js
@@ -8,7 +8,7 @@ import { execSync } from "child_process"
 
 execSync(
   `node_modules/.bin/commit-and-tag-version${
-    process.argv.includes("--alpha") ? " --prerelease alpha" : ""
+    process.argv.includes("--alpha") ? " --skip.changelog --prerelease alpha" : ""
   }${process.argv.includes("--patch") ? " --release-as patch" : ""}${
     process.argv.includes("--minor") ? " --release-as minor" : ""
   }${process.argv.includes("--major") ? " --release-as major" : ""}`
