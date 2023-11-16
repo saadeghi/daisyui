c31f44569d9c3f58ce69525b995058d9a5d6d4d4
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-16 21:18:22 +0330

    fix: missing utility classes in CDN

--- src/full/tailwind.config.cjs
+++ src/full/tailwind.config.cjs
@@ -1,14 +1,54 @@
 const plugin = require("tailwindcss/plugin")
-const responsiveRegex = require("../lib/responsiveRegex")
-const utilityClasses = require("../lib/utility-classes")
 
 module.exports = {
   content: [{ raw: '<input class="theme-controller" checked/>', extension: "html" }],
-  safelist: responsiveRegex,
+  safelist: [
+    {
+      pattern: /.*/,
+    },
+    {
+      // responsive utilites for daisyUI responsive modifiers
+      pattern: /.(sm|md|lg|xl)/,
+      variants: ["sm", "md", "lg", "xl"],
+    },
+    {
+      // responsive utilites for daisyUI components
+      pattern:
+        /(drawer-open|modal-middle|modal-top|modal-bottom|card-side|card-compact|card-normal|stats|divider-horizontal|divider-vertical)/,
+      variants: ["sm", "md", "lg", "xl"],
+    },
+    {
+      // color utilities for daisyUI colors
+      pattern:
+        /(bg|to|via|from|text|fill|stroke|border|outline)-((primary|secondary|accent|neutral)(-content|))|((info|success|warning|error)(-content|))|(base)(-100|-200|-300|-content)/,
+      variants: [
+        // "first",
+        // "last",
+        // "odd",
+        // "even",
+        // "visited",
+        // "checked",
+        // "empty",
+        // "read-only",
+        // "group-hover",
+        // "group-focus",
+        // "focus-within",
+        "hover",
+        "focus",
+        // "focus-visible",
+        // "active",
+        // "disabled",
+      ],
+    },
+  ],
   theme: {
     colors: require("../theming"),
     extend: {
-      ...utilityClasses,
+      borderRadius: {
+        badge: "var(--rounded-badge, 1.9rem)",
+        btn: "var(--rounded-btn, 0.5rem)",
+        box: "var(--rounded-box, 1rem)",
+      },
     },
   },
   corePlugins: [
