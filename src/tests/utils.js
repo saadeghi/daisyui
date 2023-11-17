8bd479d47792d683a32cf654ec1c58ce742ef125
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-17 15:10:09 +0330

    tests

--- src/tests/utils.js
+++ src/tests/utils.js
@@ -1,3 +1,7 @@
+import { exec } from "child_process"
+import fs from "fs"
+import glob from "glob"
+
 // WCAG 2.0 level AA requires a contrast ratio of at least 4.5:1
 // for normal text and 3:1 for large text. WCAG 2.1 requires a
 // contrast ratio of at least 3:1 for graphics and user interface
@@ -14,4 +18,34 @@ export const trimCssVariable = (input) => {
 
 export const trimThemeName = (input) => {
   return input.replace("[data-theme=", "").replace("]", "")
+}
+
+export async function executeCommands(commands) {
+  for (const command of commands) {
+    await new Promise((resolve) => {
+      exec(command, () => resolve())
+    })
+  }
+}
+
+export async function stringExistsInFile(filePath, searchString) {
+  const firstMatchedFile = glob.sync(filePath)[0]
+  if (!firstMatchedFile) {
+    throw new Error(`File not found: ${filePath}`)
+  }
+  const fileContent = fs.readFileSync(firstMatchedFile, "utf8")
+  return fileContent.includes(searchString)
+}
+
+export async function printPackageDependencies(name) {
+  fs.readFile(`files/${name}/package.json`, "utf8", (err, data) => {
+    if (err) throw err
+    const packageJson = JSON.parse(data)
+    const allDependencies = {
+      ...packageJson.dependencies,
+      ...packageJson.devDependencies,
+    }
+    const dependencyNames = Object.keys(allDependencies)
+    console.table(`${name} dependencies: ${dependencyNames.join(", ") || "No dependencies found."}`)
+  })
 }
