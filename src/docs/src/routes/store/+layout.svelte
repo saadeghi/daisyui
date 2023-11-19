bdb2013bc7b9a2703aad6ecc7ae50515cfca0659
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-20 01:56:35 +0330

    add LS affiliate script

--- src/docs/src/routes/store/+layout.svelte
+++ src/docs/src/routes/store/+layout.svelte
@@ -3,6 +3,12 @@
   export let data
 </script>
 
+<svelte:head>
+  <script>
+    window.lemonSqueezyAffiliateConfig = { store: "daisyui" }
+  </script>
+  <script src="https://lmsqueezy.com/affiliate.js" defer></script>
+</svelte:head>
 <div class="w-full px-4 pt-20 md:px-20" dir="ltr">
   <div class="flex flex-col items-center gap-6">
     <a href="/store" class="inline-block hover:opacity-80">
