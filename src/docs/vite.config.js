fee87bc06c151709de8c363e1d5542efdbccc5c9
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-28 14:27:29 +0330

    add rollup-plugin-visualizer

--- src/docs/vite.config.js
+++ src/docs/vite.config.js
@@ -1,9 +1,17 @@
 import path from "path"
 import { defineConfig } from "vite"
 import { sveltekit } from "@sveltejs/kit/vite"
+import { visualizer } from "rollup-plugin-visualizer"
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       $components: path.resolve("/src/components"),
