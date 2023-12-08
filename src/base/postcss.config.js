e29b6bc7a6f47b10ad68cdb6f9512d73f7f5d96f
Goran <gospaso@gmail.com>
2023-12-08 16:34:56 -0400

    fix: compile base colors

--- src/base/postcss.config.js
+++ src/base/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss/nesting"),
+    require("tailwindcss")("./src/components/tailwind.config.js"),
     // require("autoprefixer"),
   ],
 }
