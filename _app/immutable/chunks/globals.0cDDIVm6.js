77c868460e9c9303f18773e28f211e83618f0b26
saadeghi <saadeghi@users.noreply.github.com>
2023-12-16 10:39:57 +0000

    deploy: ff7c1067a1f497446be4722717dfdc23a7a7d2f3

--- _app/immutable/chunks/globals.0cDDIVm6.js
+++
@@ -1,1 +0,0 @@
-const o=typeof window<"u"?window:typeof globalThis<"u"?globalThis:global;export{o as g};
---
+++ _app/immutable/nodes/100.CHw6Kjhx.js
@@ -0,0 +1,1 @@
+import{s as n,L as s}from"../chunks/scheduler.z1v5p5l_.js";import{S as t,i as r}from"../chunks/index.wUxZk5jp.js";function a(o){return s(()=>{window.location.href="/resources/videos/"}),[]}class u extends t{constructor(e){super(),r(this,e,a,null,n,{})}}export{u as component};
