fd788997e3887e514d9517cb0b5aaa67e11a35af
GAUTAM MANAK <106014185+GAUTAMMANAK1@users.noreply.github.com>
2023-12-18 02:59:11 +0530

    Update tailwind.config.js
    add another color theme

--- src/components/tailwind.config.js
+++ src/components/tailwind.config.js
@@ -19,30 +19,30 @@ module.exports = {
       addUtilities(require("../../dist/utilities-styled"), {
         variants: ["responsive"],
       })
-      // matchUtilities(
-      //   {
-      //     text: (value) => ({
-      //       "@supports (color: lch(0 0 0))": {
-      //         color: value.replace("hsl", "lch").replace("<alpha-value>", "var(--tw-text-opacity)"),
-      //       },
-      //     }),
-      //     bg: (value) => ({
-      //       "@supports (color: lch(0 0 0))": {
-      //         backgroundColor: value
-      //           .replace("hsl", "lch")
-      //           .replace("<alpha-value>", "var(--tw-bg-opacity)"),
-      //       },
-      //     }),
-      //     border: (value) => ({
-      //       "@supports (color: lch(0 0 0))": {
-      //         borderColor: value
-      //           .replace("hsl", "lch")
-      //           .replace("<alpha-value>", "var(--tw-border-opacity)"),
-      //       },
-      //     }),
-      //   },
-      //   { values: theme("colors") }
-      // )
+    matchUtilities(
+       {
+        text: (value) => ({=
+          "@supports (color: lch(0 0 0))": {
+             color: value.replace("hsl", "lch").replace("<alpha-value>", "var(--tw-text-opacity)"),
+         },
+    }),
+     bg: (value) => ({
+         "@supports (color: lch(0 0 0))": {
+               backgroundColor: value
+                 .replace("hsl", "lch")
+                 .replace("<alpha-value>", "var(--tw-bg-opacity)"),
+             },
+         }),
+           border: (value) => ({
+             "@supports (color: lch(0 0 0))": {
+               borderColor: value
+                 .replace("hsl", "lch")
+                 .replace("<alpha-value>", "var(--tw-border-opacity)"),
+             },
+           }),
+         },
+        { values: theme("colors") }
+       )
     }),
   ],
 }
