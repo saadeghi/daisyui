700ba07434170fcc414bc297eca13667c334b7e6
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-12-19 12:53:18 +0300

    store

--- src/docs/src/routes/store/+layout.svelte
+++ src/docs/src/routes/store/+layout.svelte
@@ -11,12 +11,10 @@
 </svelte:head>
 <div class="w-full px-4 pt-20 md:px-20" dir="ltr">
   <div class="flex flex-col items-center gap-6">
-    <a href="/store" class="inline-block hover:opacity-80">
-      <h1 class="font-title text-base-content text-3xl font-extrabold lg:text-4xl xl:text-6xl">
-        daisyUI store
-      </h1>
-    </a>
-    <p class="text-base-content/60 text-xs italic">Professional templates made by daisyUI</p>
+    <h1 class="font-title text-base-content text-3xl font-extrabold lg:text-4xl xl:text-6xl">
+      daisyUI store
+    </h1>
+    <p class="text-base-content/60 text-xs italic">Official templates made by daisyUI</p>
   </div>
   <div>
     <slot />
