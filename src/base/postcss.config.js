ae4807b94408453ecd0f373c6343572efc984b16
Goran <gospaso@gmail.com>
2023-12-20 14:34:03 -0400

    fix: #2634 compile base colors (#2635)
    * fix: compile base colors
    
    * fix: compile base colors
    
    dir typo
    
    * fix: add tailwind config to base

--- src/base/postcss.config.js
+++ src/base/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss/nesting"),
+    require("tailwindcss")("./src/base/tailwind.config.js"),
     // require("autoprefixer"),
   ],
 }
