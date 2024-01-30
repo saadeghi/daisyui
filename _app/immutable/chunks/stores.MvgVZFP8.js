20ec8a7b9f10e7212de74568bf3a7a2b4a7262be
saadeghi <saadeghi@users.noreply.github.com>
2024-01-30 04:47:42 +0000

    deploy: d425e2906a6681d4254e17c035f536931497ff27

--- _app/immutable/chunks/stores.MvgVZFP8.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./singletons.N8bs6n_M.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
