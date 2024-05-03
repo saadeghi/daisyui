daa5942d59a80222fd8c64eaded5578510858c98
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-05-03 21:51:56 +0300

    docs: update Tailwind Play link

--- src/docs/src/routes/(redirects)/tailwindplay/+page.server.js
+++ src/docs/src/routes/(redirects)/tailwindplay/+page.server.js
@@ -1,5 +1,5 @@
 export const ssr = false
 import { redirect } from "@sveltejs/kit"
 export function load() {
-  redirect(301, "https://play.tailwindcss.com/IIWW8U8Zeq");
+  redirect(301, "https://play.tailwindcss.com/WFjtZtmDJM")
 }
