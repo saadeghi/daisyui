c044622352da9f11d3b8fb82fb67184267b6009d
saadeghi <saadeghi@users.noreply.github.com>
2024-06-27 23:54:35 +0000

    deploy: 6a4a7354eb7eb46db75f4800ab3723ec200b1f9c

---
+++ _app/immutable/entry/start.DC9tw0Q8.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C4Af2mbV.js";export{t as start};
---
+++ _app/immutable/nodes/103.9-_9xKLP.js
@@ -0,0 +1,14 @@
+import{h as s,$ as c,f as n,s as a,a as m,d as f}from"../chunks/disclose-version.BHiWwTRW.js";var l=f(`<meta name="robots" content="noindex"> <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"> <script>
+const urlParams = new URLSearchParams(window.location.search)
+window.lemonSqueezyAffiliateConfig = {
+  store: "daisyui",
+  debug: true,
+  // trackOnLoad: false,
+  // onReady: (e) => {
+  //   e.Track(urlParams.get("aff"))
+  // },
+  onTrack: (e) => {
+    // window.location.href = \`https://daisyui.lemonsqueezy.com/checkout/buy/\${urlParams.get("product")}?aff_ref=\${e.click}\`
+  },
+}
+<\/script> <script src="https://lmsqueezy.com/affiliate.js" defer=""><\/script>`,1);function h(u){s(t=>{var e=l();c.title="Loading…";var r=n(e),i=a(a(r,!0)),o=a(a(i,!0)),d=a(a(o,!0));m(t,e)})}export{h as component};
