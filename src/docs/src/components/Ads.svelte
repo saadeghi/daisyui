b3275860f67c688d2f17e912bdd9a8145ec95c9e
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-06-27 03:40:51 +0300

    docs

--- src/docs/src/components/Ads.svelte
+++ src/docs/src/components/Ads.svelte
@@ -1,118 +1,49 @@
 <script>
 import { onMount } from "svelte"
-import { readEnv } from "$lib/util"
-export let adslot
+import { fade } from "svelte/transition"
 import Sponsors from "$components/Sponsors.svelte"
+export let adslot
+
+let showCarbon = false
+onMount(async () => {
+  setTimeout(() => {
+    showCarbon = true
+  }, 1000)
+})
 </script>
 
-{#if adslot === "adesense-1"}
-  <div class="mx-auto mb-6 flex h-[100px] w-[300px] items-center justify-center md:hidden">
-    {#if readEnv("VITE_ADS") == "true"}
-      <!-- daisyui-300-100 -->
-      <ins
-        class="adsbygoogle"
-        style="display:inline-block;width:300px;height:100px"
-        data-ad-client="ca-pub-4812562253949561"
-        data-ad-slot="6427020085">
-      </ins>
-      <script>
-        ;(adsbygoogle = window.adsbygoogle || []).push({})
-      </script>
-    {/if}
-  </div>
-{:else if adslot === "adesense-2"}
-  <div class="mx-auto mb-6 hidden h-[90px] w-[728px] items-center justify-center md:flex lg:hidden">
-    {#if readEnv("VITE_ADS") == "true"}
-      <!-- daisyui-728-90 -->
-      <ins
-        class="adsbygoogle"
-        style="display:inline-block;width:728px;height:90px"
-        data-ad-client="ca-pub-4812562253949561"
-        data-ad-slot="1141251424">
-      </ins>
-      <script>
-        ;(adsbygoogle = window.adsbygoogle || []).push({})
-      </script>
-    {/if}
-  </div>
-{:else if adslot === "adesense-3"}
-  <div class="mx-auto mb-6 hidden h-[90px] w-[468px] items-center justify-center lg:flex xl:hidden">
-    {#if readEnv("VITE_ADS") == "true"}
-      <!-- daisyui-468-90 -->
-      <ins
-        class="adsbygoogle"
-        style="display:inline-block;width:468px;height:90px"
-        data-ad-client="ca-pub-4812562253949561"
-        data-ad-slot="9789998609">
-      </ins>
-      <script>
-        ;(adsbygoogle = window.adsbygoogle || []).push({})
-      </script>
-    {/if}
-  </div>
-{:else if adslot === "adesense-4"}
-  <div>
-    <div class="sticky top-24 mt-32">
-      <div
-        class="mx-auto hidden h-[600px] w-[160px] items-center justify-center xl:flex 2xl:hidden">
-        {#if readEnv("VITE_ADS") == "true"}
-          <!-- daisyui-160-600 -->
-          <ins
-            class="adsbygoogle"
-            style="display:inline-block;width:160px;height:600px"
-            data-ad-client="ca-pub-4812562253949561"
-            data-ad-slot="8476916933">
-          </ins>
-          <script>
-            ;(adsbygoogle = window.adsbygoogle || []).push({})
-          </script>
-        {/if}
-      </div>
-    </div>
-  </div>
-{:else if adslot === "adesense-5"}
-  <div>
-    <div class="sticky top-24 mt-32">
-      <div class="mx-auto hidden h-[600px] w-[300px] items-center justify-center 2xl:flex">
-        {#if readEnv("VITE_ADS") == "true"}
-          <!-- daisyui-300-600 -->
-          <ins
-            class="adsbygoogle"
-            style="display:inline-block;width:300px;height:600px"
-            data-ad-client="ca-pub-4812562253949561"
-            data-ad-slot="7163835260">
-          </ins>
-          <script>
-            ;(adsbygoogle = window.adsbygoogle || []).push({})
-          </script>
-        {/if}
-      </div>
-    </div>
-  </div>
-{:else if adslot === "carbon1"}
-  <div class="pointer-events-auto h-[100px] w-full min-w-[330px] max-w-[350px]">
+{#if adslot === "carbon1" && showCarbon}
+  <div
+    transition:fade={{ delay: 300 }}
+    class="pointer-events-auto h-[100px] w-full min-w-[330px] max-w-[350px]"
+  >
     {#if onMount}
       <script
         async
         type="text/javascript"
         src="//cdn.carbonads.com/carbon.js?serve=CEAI423U&placement=daisyuicom"
-        id="_carbonads_js"></script>
+        id="_carbonads_js"
+      ></script>
     {/if}
   </div>
-{:else if adslot === "carbon2"}
+{/if}
+{#if adslot === "carbon2"}
   <div>
     <div class="sticky top-20 my-6 xl:mx-2 xl:my-0 xl:mt-32 xl:w-[130px]">
       <div class="hidden xl:block">
         <Sponsors />
       </div>
       <div class="h-6"></div>
-      <div class="carbonads-responsive mx-auto flex min-h-[100px] items-center justify-center">
+      <div
+        class="carbonads-responsive mx-auto flex min-h-[100px] items-center justify-center"
+      >
         {#if onMount}
           <script
             async
             type="text/javascript"
             src="//cdn.carbonads.com/carbon.js?serve=CEAI423U&placement=daisyuicom"
-            id="_carbonads_js"></script>
+            id="_carbonads_js"
+          ></script>
         {/if}
       </div>
     </div>
