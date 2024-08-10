754fe4d6adc910eac8b8a70a33eac879811e416c
TheBrenny <iam@justbrenny.me>
2024-08-10 17:30:32 +0930

    adds cat

--- src/build.js
+++ src/build.js
@@ -1,6 +1,7 @@
 const { exec } = require("node:child_process")
 const isWin = process.platform === "win32";
 const ext = isWin ? ".cmd" : "";
+const cat = isWin ? "type" : "cat";
 
 function runCommand(command) {
     if(isWin) {
@@ -24,13 +25,13 @@ async function executeCommands() {
     await runCommand(
       "node_modules/.bin/postcss"+ext+" --config src/base src/base/*.css --base src --dir dist"
     )
-    await runCommand("cat dist/base/*.css > dist/base.css")
+    await runCommand(cat+" dist/base/*.css > dist/base.css")
     await runCommand("node_modules/.bin/prejss-cli"+ext+" dist/base.css --format commonjs")
     // await runCommand("mv dist/base.js dist/base.cjs")
     await runCommand(
       "node_modules/.bin/postcss"+ext+" --config src/utilities/global src/utilities/global/*.css --base src --dir dist"
     )
-    await runCommand("cat dist/utilities/global/*.css > dist/utilities.css")
+    await runCommand(cat+" dist/utilities/global/*.css > dist/utilities.css")
     await runCommand("node_modules/.bin/prejss-cli"+ext+" dist/utilities.css --format commonjs")
     // await runCommand("mv dist/utilities.js dist/utilities.cjs")
 
@@ -38,22 +39,22 @@ async function executeCommands() {
       "node_modules/.bin/postcss"+ext+" --config src/utilities/unstyled src/utilities/unstyled/*.css --base src --dir dist",
       { stdio: [] }
     )
-    await runCommand("cat dist/utilities/unstyled/*.css > dist/utilities-unstyled.css")
+    await runCommand(cat+" dist/utilities/unstyled/*.css > dist/utilities-unstyled.css")
     await runCommand("node_modules/.bin/prejss-cli"+ext+" dist/utilities-unstyled.css --format commonjs")
     // await runCommand("mv dist/utilities-unstyled.js dist/utilities-unstyled.cjs")
     await runCommand(
       "node_modules/.bin/postcss"+ext+" --config src/utilities/styled src/utilities/styled/*.css --base src --dir dist",
       { stdio: [] }
     )
-    await runCommand("cat dist/utilities/styled/*.css > dist/utilities-styled.css")
+    await runCommand(cat+" dist/utilities/styled/*.css > dist/utilities-styled.css")
     await runCommand("node_modules/.bin/prejss-cli dist/utilities-styled.css --format commonjs")
     // await runCommand("mv dist/utilities-styled.js dist/utilities-styled.cjs")
     await runCommand(
       "node_modules/.bin/postcss"+ext+" --config src/components src/components/**/*.css --base src --dir dist"
     )
-    await runCommand("cat dist/components/unstyled/*.css > dist/unstyled.css")
+    await runCommand(cat+" dist/components/unstyled/*.css > dist/unstyled.css")
     await runCommand(
-      "cat dist/components/unstyled/*.css dist/components/styled/*.css > dist/styled.css"
+      cat+" dist/components/unstyled/*.css dist/components/styled/*.css > dist/styled.css"
     )
     await runCommand("node_modules/.bin/prejss-cli"+ext+" dist/{unstyled,styled}.css --format commonjs")
     // await runCommand("mv dist/unstyled.js dist/unstyled.cjs")
