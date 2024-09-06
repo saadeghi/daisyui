d066a1672a6ce02818e859347382b8ad70272870
マルコメ <akio.muto@bnt.benextgroup.jp>
2024-09-06 09:40:41 +0900

    docs: use webp image on tailwindplay (#3200)

--- src/docs/src/routes/(redirects)/tailwindplay/+page.server.js
+++ src/docs/src/routes/(redirects)/tailwindplay/+page.server.js
@@ -1,5 +1,5 @@
 export const ssr = false
 import { redirect } from "@sveltejs/kit"
 export function load() {
-  redirect(301, "https://play.tailwindcss.com/IvZT5sJSsa")
+  redirect(301, "https://play.tailwindcss.com/wWndQ9FxeZ")
 }
