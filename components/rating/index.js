7aa74e627078f8594d8f52b0adfc9a698b9d08be
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-09-10 02:56:51 +0300

    .

---
+++ components/rating/index.js
@@ -0,0 +1,78 @@
+export default {
+  ".rating": {
+    "@apply relative inline-flex": {},
+    "& input": {
+      "@apply appearance-none border-none": {},
+      "WebkitAppearance": "none"
+    },
+    ":where(input)": {
+      "animation": "rating-pop var(--animation-input, 0.25s) ease-out",
+      "@apply cursor-pointer rounded-none bg-base-content h-6 w-6 opacity-100": {}
+    },
+    "& .rating-hidden": {
+      "@apply w-2 bg-transparent": {}
+    },
+    "input[type=\"radio\"]:checked": {
+      "backgroundImage": "none"
+    },
+    "input:checked ~ input,\n  input[aria-checked=\"true\"] ~ input": {
+      "@apply opacity-20": {}
+    },
+    "input:focus-visible": {
+      "@apply transition-transform duration-300 ease-out": {},
+      "transform": "translateY(-0.125em)"
+    },
+    "& input:active:focus": {
+      "animation": "none",
+      "transform": "translateY(-0.125em)"
+    },
+    "&:is(.rating-xs)": {
+      "input": {
+        "@apply size-3": {}
+      }
+    },
+    "&:is(.rating-sm)": {
+      "input": {
+        "@apply size-4": {}
+      }
+    },
+    "&:is(.rating-md)": {
+      "input": {
+        "@apply size-6": {}
+      }
+    },
+    "&:is(.rating-lg)": {
+      "input": {
+        "@apply size-10": {}
+      }
+    }
+  },
+  ".rating-half": {
+    ":where(input:not(.rating-hidden))": {
+      "@apply w-3": {}
+    },
+    "&.rating-xs input:not(.rating-hidden)": {
+      "@apply w-1.5": {}
+    },
+    "&.rating-sm input:not(.rating-hidden)": {
+      "@apply w-2": {}
+    },
+    "&.rating-md input:not(.rating-hidden)": {
+      "@apply w-3": {}
+    },
+    "&.rating-lg input:not(.rating-hidden)": {
+      "@apply w-5": {}
+    }
+  },
+  "@keyframes rating-pop": {
+    "0%": {
+      "transform": "translateY(-0.125em)"
+    },
+    "40%": {
+      "transform": "translateY(-0.125em)"
+    },
+    "100%": {
+      "transform": "translateY(0)"
+    }
+  }
+};
