573b7d49a74e1d3c349a64a0f941bc58cb83be73
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-09-11 19:28:19 +0300

    prefix

--- functions/cssToJs.js
+++ functions/cssToJs.js
@@ -1,11 +1,14 @@
 import { promises as fs } from "node:fs"
 import postcss from "postcss"
 import postcssJs from "postcss-js"
+import prefixer from "postcss-prefixer"
 import { replaceApplyTrueWithEmptyObject } from "./replaceApplyTrueWithEmptyObject.js"
 
 export const cssToJs = async (cssFile) => {
 	const cssContent = await fs.readFile(cssFile, "utf-8")
-	const root = postcss.parse(cssContent)
+	// const prefixed = postcss([prefixer({ prefix: "dz-" })]).process(cssContent).css
+	const prefixed = cssContent
+	const root = postcss.parse(prefixed)
 	const jsContent = postcssJs.objectify(root)
 	replaceApplyTrueWithEmptyObject(jsContent)
 	return JSON.stringify(jsContent, null, 2)
