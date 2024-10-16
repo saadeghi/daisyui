09f7c7bf0de4fdf323cdcc1f0a9922658c67b56c
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-10-16 18:07:40 +0300

    include required tokens by default

--- functions/generateIndexjs.js
+++ functions/generateIndexjs.js
@@ -15,7 +15,7 @@ const generateJSContent = async () => {
     base.forEach(item => {
       const importName = `${item}`;
       imports += `import ${importName} from './base/${item}';\n`;
-      addBaseContent += `  ${importName}({ addBase });\n`;
+      addBaseContent += `      ${importName}({ addBase });\n`;
     });
 
     // Add component imports and content
@@ -23,7 +23,7 @@ const generateJSContent = async () => {
     components.forEach(item => {
       const importName = `${item}`;
       imports += `import ${importName} from './components/${item}';\n`;
-      addComponentsContent += `  ${importName}({ addComponents });\n`;
+      addComponentsContent += `      ${importName}({ addComponents });\n`;
     });
 
     // Add utilities imports and content
@@ -31,17 +31,70 @@ const generateJSContent = async () => {
     utilities.forEach(item => {
       const importName = `${item}`;
       imports += `import ${importName} from './utilities/${item}';\n`;
-      addUtilitiesContent += `  ${importName}({ addUtilities });\n`;
+      addUtilitiesContent += `      ${importName}({ addUtilities });\n`;
     });
 
     const content = `${imports}
 console.log('/*! 🌼 daisyUI ${packageJson.version} */')
-export default ({ addBase, addComponents, addUtilities }) => {
+const plugin = {
+  withOptions: (pluginFunction, configFunction = () => ({})) => {
+    const optionsFunction = (options) => ({
+      handler: pluginFunction(options),
+      config: configFunction(options),
+    });
+    optionsFunction.__isOptionsFunction = true;
+    return optionsFunction;
+  }
+};
+export default plugin.withOptions(
+  (options = {}) => {
+    return ({ addBase, addComponents, addUtilities }) => {
 ${addBaseContent}
 ${addComponentsContent}
-${addUtilitiesContent}}
+${addUtilitiesContent}
+    }
+  },
+  (options) => {
+    return {
+      theme: {
+        extend: {
+          colors: {
+            'base-100': 'oklch(100% 0 0)',
+            'base-200': 'oklch(96.1151% 0 0)',
+            'base-300': 'oklch(92.4169% 0.00108 197.137559)',
+            'base-content': 'oklch(27.8078% 0.029596 256.84795)',
+            'primary': 'oklch(49.12% 0.3096 275.75)',
+            'primary-content': 'oklch(89.824% 0.06192 275.75)',
+            'secondary': 'oklch(69.71% 0.329 342.55)',
+            'secondary-content': 'oklch(98.71% 0.0106 342.55)',
+            'accent': 'oklch(76.76% 0.184 183.61)',
+            'accent-content': 'oklch(15.352% 0.0368 183.61)',
+            'neutral': 'oklch(20% 0.02476 255.701624)',
+            'neutral-content': 'oklch(89.4994% 0.011585 252.096176)',
+            'info': 'oklch(72.06% 0.191 231.6)',
+            'info-content': 'oklch(0% 0 0)',
+            'success': 'oklch(64.8% 0.15 160)',
+            'success-content': 'oklch(0% 0 0)',
+            'warning': 'oklch(84.71% 0.199 83.87)',
+            'warning-content': 'oklch(0% 0 0)',
+            'error': 'oklch(71.76% 0.221 22.18)',
+            'error-content': 'oklch(0% 0 0)',
+          },
+          borderRadius: {
+            'badge': '1.9rem',
+            'btn': '0.5rem',
+            'box': '1rem',
+          },
+          borderWidth: {
+            'button': '1px',
+            'tab': '1px',
+          }
+        }
+      }
+    }
+  }
+)
 `;
-
     return { content };
   } catch (error) {
     throw new Error(`Failed to generate JS content: ${error.message}`);
