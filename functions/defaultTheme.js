252d83984865e0aa54401aa4d61860320c96bdf9
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-10-16 18:32:27 +0300

    add `logs` option
    ```css
    @plugin "daisyui" {
      logs: true;
    }
    ```

---
+++ functions/defaultTheme.js
@@ -0,0 +1,33 @@
+export default {
+  colors: {
+    'base-100': 'oklch(100% 0 0)',
+    'base-200': 'oklch(96.1151% 0 0)',
+    'base-300': 'oklch(92.4169% 0.00108 197.137559)',
+    'base-content': 'oklch(27.8078% 0.029596 256.84795)',
+    'primary': 'oklch(49.12% 0.3096 275.75)',
+    'primary-content': 'oklch(89.824% 0.06192 275.75)',
+    'secondary': 'oklch(69.71% 0.329 342.55)',
+    'secondary-content': 'oklch(98.71% 0.0106 342.55)',
+    'accent': 'oklch(76.76% 0.184 183.61)',
+    'accent-content': 'oklch(15.352% 0.0368 183.61)',
+    'neutral': 'oklch(20% 0.02476 255.701624)',
+    'neutral-content': 'oklch(89.4994% 0.011585 252.096176)',
+    'info': 'oklch(72.06% 0.191 231.6)',
+    'info-content': 'oklch(0% 0 0)',
+    'success': 'oklch(64.8% 0.15 160)',
+    'success-content': 'oklch(0% 0 0)',
+    'warning': 'oklch(84.71% 0.199 83.87)',
+    'warning-content': 'oklch(0% 0 0)',
+    'error': 'oklch(71.76% 0.221 22.18)',
+    'error-content': 'oklch(0% 0 0)',
+  },
+  borderRadius: {
+    'badge': '1.9rem',
+    'btn': '0.5rem',
+    'box': '1rem',
+  },
+  borderWidth: {
+    'button': '1px',
+    'tab': '1px',
+  }
+}
