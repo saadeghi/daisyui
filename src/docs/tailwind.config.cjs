582faabc2cffb8db3e21325d271f51efd51fada8
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-10-26 16:55:10 +0300

    .

--- src/docs/tailwind.config.cjs
+++ src/docs/tailwind.config.cjs
@@ -1,20 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts,svx,md,json}"],
-  safelist: [
-    "mr-1",
-    "opacity-20",
-    "hover:opacity-60",
-    "text-base",
-    "font-bold",
-    "inline-block",
-    "align-middle",
-    "relative",
-    "-mt-1",
-    "[&_svg]:visible",
-  ],
   theme: {
     extend: {
-      colors: require("../theming"),
       fontFamily: {
         title: ["Figtree", "Noto Sans JP", "Noto Sans", "Vazirmatn", "sans-serif"],
         sans: [
@@ -48,16 +35,7 @@ module.exports = {
       },
     },
   },
-  daisyui: {
-    logs: false,
-    themes: true,
-  },
   plugins: [
     require("@tailwindcss/typography"),
-    process.env.NODE_ENV === "production" ? require("daisyui") : [],
-  ],
-  presets: [
-    process.env.NODE_ENV === "production" ? [] : require("../components/tailwind.config.js"),
-    "node_modules/tailwindcss/stubs/defaultConfig.stub.js",
   ],
 }
