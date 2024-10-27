a1180067b14b233dc7ba2419973d32a20f874273
Innei <tukon479@gmail.com>
2024-10-28 00:26:24 +0800

    update
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/theming/index.js
+++ src/theming/index.js
@@ -32,7 +32,17 @@ const colorObject = {
 
   neutral: {
     DEFAULT:  "var(--fallback-n,oklch(var(--n)/<alpha-value>))", 
-    ... colors.neutral,
+    50: "#fafafa",
+    100: "#f5f5f5",
+    200: "#e5e5e5",
+    300: "#d4d4d4",
+    400: "#a3a3a3",
+    500: "#737373",
+    600: "#525252",
+    700: "#404040",
+    800: "#262626",
+    900: "#171717",
+    950: "#0a0a0a"
   },
   "neutral-content": "var(--fallback-nc,oklch(var(--nc)/<alpha-value>))",
 }
