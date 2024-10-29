c0ce776a1259190e39b738faf49df7a235fc68ef
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-10-30 01:23:32 +0300

    remove layers from chunks.css to prevent higher css specificity of tailwind CDN file

--- functions/generateChunks.js
+++ functions/generateChunks.js
@@ -6,35 +6,36 @@ import themeOrder from './themeOrder';
 export const generateChunks = async (filename) => {
   try {
     let content = '';
+    // let content = '@layer base, themes, components, utilities;\n';
     // content += `@import url(https://cdn.jsdelivr.net/npm/tailwindcss@next/preflight.min.css) layer(base);\n`;
 
     const themes = await getDirectoriesWithTargetFile('./theme', 'index.css');
     const allowedThemes = ['light', 'dark'];
     themeOrder.forEach(theme => {
       if (themes.includes(theme) && allowedThemes.includes(theme)) {
-        content += `@import url(theme/${theme}/index.css) layer(themes);\n`;
+        content += `@import url(theme/${theme}/index.css);\n`;
       }
     });
 
     const baseFiles = await getFileNames('./base', ".css", false);
     baseFiles.forEach(filePath => {
-      content += `@import url(base/${filePath}.css) layer(base);\n`;
+      content += `@import url(base/${filePath}.css);\n`;
     });
 
     const componentFiles = await getFileNames('./components', ".css", false);
     componentFiles.forEach(filePath => {
-      content += `@import url(components/${filePath}.css) layer(components);\n`;
+      content += `@import url(components/${filePath}.css);\n`;
     });
 
     const utilityFiles = await getFileNames('./utilities', ".css", false);
     utilityFiles.forEach(filePath => {
-      content += `@import url(utilities/${filePath}.css) layer(utilities);\n`;
+      content += `@import url(utilities/${filePath}.css);\n`;
     });
 
     // Load color files with specific ordering
     const colorFiles = await getFileNames('./colors', ".css", false);
     colorFiles.forEach(filePath => {
-      content += `@import url(colors/${filePath}.css) layer(utilities);\n`;
+      content += `@import url(colors/${filePath}.css);\n`;
     });
 
     // Write to file
