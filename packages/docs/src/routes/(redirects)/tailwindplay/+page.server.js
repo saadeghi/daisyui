7705eca80068162d8b70a1887a021ee172954fd3
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-12-31 14:12:18 +0300

    docs

--- packages/docs/src/routes/(redirects)/tailwindplay/+page.server.js
+++ packages/docs/src/routes/(redirects)/tailwindplay/+page.server.js
@@ -1,5 +1,5 @@
 export const ssr = false
 import { redirect } from "@sveltejs/kit"
 export function load() {
-  redirect(301, "https://play.tailwindcss.com/wWndQ9FxeZ")
+  redirect(301, "https://play.tailwindcss.com/dpFNMGDJU8")
 }
