4da804255e88a8d9de4fe15999010e32fb91bc7a
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-01-22 23:47:41 +0300

    docs

--- packages/docs/src/routes/(routes)/+layout.svelte
+++ packages/docs/src/routes/(routes)/+layout.svelte
@@ -12,7 +12,7 @@
   let { data, children } = $props()
 
   onNavigate((navigation) => {
-    track("G-ER9PXT2JR4")
+    track("G-10F40JCSMZ")
     if (!document.startViewTransition) return
 
     return new Promise((resolve) => {
