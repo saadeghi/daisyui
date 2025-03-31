7acedd374a75424e57c6d420bbd41c53da59576a
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-03-31 23:15:43 +0300

    docs

--- packages/docs/src/lib/util.js
+++ packages/docs/src/lib/util.js
@@ -1,11 +1,11 @@
-export const slugFromPath = (path) => path.match(/([\w-]+)\.(svelte\.md|md|svx)/i)?.[1] ?? null
+// export const slugFromPath = (path) => path.match(/([\w-]+)\.(svelte\.md|md|svx)/i)?.[1] ?? null
 
-export const readEnv = (id, defaultValue = "") => {
-  if (import.meta.env[id] !== undefined) {
-    return import.meta.env[id]
-  }
-  return defaultValue
-}
+// export const readEnv = (id, defaultValue = "") => {
+//   if (import.meta.env[id] !== undefined) {
+//     return import.meta.env[id]
+//   }
+//   return defaultValue
+// }
 
 export const getOS = () => {
   const userAgent = window.navigator.userAgent.toLowerCase()
