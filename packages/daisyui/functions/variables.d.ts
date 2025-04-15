ca98cafa2edeaad6dedff756bbb06c7d21a5d1a8
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-04-15 15:41:31 +0300

    add types for functions/variables.js

---
+++ packages/daisyui/functions/variables.d.ts
@@ -0,0 +1,32 @@
+declare module 'daisyui/functions/variables' {
+  const variables: {
+    colors: {
+      "base-100": string;
+      "base-200": string;
+      "base-300": string;
+      "base-content": string;
+      primary: string;
+      "primary-content": string;
+      secondary: string;
+      "secondary-content": string;
+      accent: string;
+      "accent-content": string;
+      neutral: string;
+      "neutral-content": string;
+      info: string;
+      "info-content": string;
+      success: string;
+      "success-content": string;
+      warning: string;
+      "warning-content": string;
+      error: string;
+      "error-content": string;
+    };
+    borderRadius: {
+      selector: string;
+      field: string;
+      box: string;
+    };
+  };
+  export default variables;
+}
