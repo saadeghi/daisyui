fb19efda66c8fe962897b36b1195edf428855e41
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-04-22 01:32:02 +0300

    docs

--- packages/docs/src/components/Footer.svelte
+++ packages/docs/src/components/Footer.svelte
@@ -127,6 +127,21 @@
             >
           </a>
           <a
+            title="X"
+            target="_blank"
+            rel="noopener, noreferrer"
+            href="https://x.com/daisyui_"
+            class="opacity-50 hover:opacity-100"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+              <path
+                fill="currentColor"
+                d="M18.42,14.009L27.891,3h-2.244l-8.224,9.559L10.855,3H3.28l9.932,14.455L3.28,29h2.244l8.684-10.095,6.936,10.095h7.576l-10.301-14.991h0Zm-3.074,3.573l-1.006-1.439L6.333,4.69h3.447l6.462,9.243,1.006,1.439,8.4,12.015h-3.447l-6.854-9.804h0Z"
+              >
+              </path>
+            </svg>
+          </a>
+          <a
             title="Unpkg CDN"
             target="_blank"
             rel="noopener, noreferrer"
