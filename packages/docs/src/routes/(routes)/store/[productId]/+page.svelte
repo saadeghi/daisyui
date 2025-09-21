68924f3c349de9f715b90c9dd21d7a4d0c46a536
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-09-22 00:23:42 +0300

    store

--- packages/docs/src/routes/(routes)/store/[productId]/+page.svelte
+++ packages/docs/src/routes/(routes)/store/[productId]/+page.svelte
@@ -366,39 +366,97 @@
     <div class="xl:col-span-5">
       <div class="sticky top-20 flex flex-col gap-6">
         <div class="flex flex-col gap-2">
-          {#if data.product.badge}
-            <span
-              class={`badge  badge-soft flex gap-1 rounded-full ps-1 text-xs shadow-xs ${data.product.badge.class}`}
-            >
-              {#if data.product.badge.icon === "check"}
-                <svg class="size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                  <g fill="none">
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15ZM11.8435 6.20859C12.0967 5.88082 12.0363 5.40981 11.7086 5.15654C11.3808 4.90327 10.9098 4.96365 10.6565 5.29141L6.95615 10.0801L5.30747 8.24828C5.03038 7.94039 4.55616 7.91543 4.24828 8.19253C3.94039 8.46962 3.91544 8.94384 4.19253 9.25172L6.44253 11.7517C6.59132 11.917 6.80582 12.0078 7.02809 11.9995C7.25036 11.9911 7.45746 11.8846 7.59346 11.7086L11.8435 6.20859Z"
-                      fill="currentColor"
-                    >
-                    </path>
-                  </g>
-                </svg>
-              {/if}
-              {#if data.product.badge.icon === "wait"}
-                <svg class="size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                  <g fill="none">
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8ZM8.75 3.75C8.75 3.33579 8.41421 3 8 3C7.58579 3 7.25 3.33579 7.25 3.75V8C7.25 8.41421 7.58579 8.75 8 8.75H11.25C11.6642 8.75 12 8.41421 12 8C12 7.58579 11.6642 7.25 11.25 7.25H8.75V3.75Z"
-                      fill="currentColor"
-                    >
-                    </path>
-                  </g>
-                </svg>
-              {/if}
-              {data.product.badge.text}
-            </span>
-          {/if}
+          <div class="flex gap-2">
+            {#if data.product.changelog?.length}
+              <span
+                class="tooltip"
+                data-tip={`${data.product.changelog?.[0]?.changes?.length || 0} new changes`}
+              >
+                <button
+                  class="btn btn-xs btn-success btn-soft rounded-full"
+                  onclick={() => changelog.showModal()}
+                >
+                  <span class="status status-success"></span>
+                  Version {data.product.changelog?.[0]?.version || "0"}
+                </button>
+              </span>
+              <dialog id="changelog" class="modal max-md:modal-bottom">
+                <div
+                  class="modal-box max-h-[80vh] w-full max-w-[50rem] max-md:max-h-[80vh] lg:p-20"
+                >
+                  <h3 class="font-title text-lg font-semibold">Changelog</h3>
+                  <div class="py-4">
+                    {#if data.product.changelog}
+                      <div class="overflow-x-auto">
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th>Version</th>
+                              <th>Changes</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {#each data.product.changelog as version}
+                              <tr>
+                                <td class="align-top font-semibold whitespace-nowrap"
+                                  >v{version.version}</td
+                                >
+                                <td>
+                                  <ul class="list-inside list-disc space-y-1">
+                                    {#each version.changes as change}
+                                      <li class="text-sm">{change}</li>
+                                    {/each}
+                                  </ul>
+                                </td>
+                              </tr>
+                            {/each}
+                          </tbody>
+                        </table>
+                      </div>
+                    {:else}
+                      <p class="text-base-content/60">No changelog available.</p>
+                    {/if}
+                  </div>
+                </div>
+                <form method="dialog" class="modal-backdrop">
+                  <button>close</button>
+                </form>
+              </dialog>
+            {/if}
+            {#if data.product.badge}
+              <span
+                class={`badge badge-soft flex gap-1 rounded-full ps-1 text-xs shadow-xs ${data.product.badge.class}`}
+              >
+                {#if data.product.badge.icon === "check"}
+                  <svg class="size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                    <g fill="none">
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15ZM11.8435 6.20859C12.0967 5.88082 12.0363 5.40981 11.7086 5.15654C11.3808 4.90327 10.9098 4.96365 10.6565 5.29141L6.95615 10.0801L5.30747 8.24828C5.03038 7.94039 4.55616 7.91543 4.24828 8.19253C3.94039 8.46962 3.91544 8.94384 4.19253 9.25172L6.44253 11.7517C6.59132 11.917 6.80582 12.0078 7.02809 11.9995C7.25036 11.9911 7.45746 11.8846 7.59346 11.7086L11.8435 6.20859Z"
+                        fill="currentColor"
+                      >
+                      </path>
+                    </g>
+                  </svg>
+                {/if}
+                {#if data.product.badge.icon === "wait"}
+                  <svg class="size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                    <g fill="none">
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8ZM8.75 3.75C8.75 3.33579 8.41421 3 8 3C7.58579 3 7.25 3.33579 7.25 3.75V8C7.25 8.41421 7.58579 8.75 8 8.75H11.25C11.6642 8.75 12 8.41421 12 8C12 7.58579 11.6642 7.25 11.25 7.25H8.75V3.75Z"
+                        fill="currentColor"
+                      >
+                      </path>
+                    </g>
+                  </svg>
+                {/if}
+                {data.product.badge.text}
+              </span>
+            {/if}
+          </div>
 
           <h1 class="font-title text-4xl font-bold">{data.product.title}</h1>
         </div>
@@ -556,7 +614,7 @@
 
         <dialog class="modal max-md:modal-bottom" bind:this={licenseDialog}>
           <div class="modal-box max-h-[80vh] w-full max-w-[50rem] max-md:max-h-[80vh] lg:p-20">
-            <h3 class="text-lg font-bold">{data.product.title} License</h3>
+            <h3 class="font-title text-lg font-semibold">{data.product.title} License</h3>
             <pre class="py-4 text-xs whitespace-pre-wrap">{licenseContent}</pre>
           </div>
           <form method="dialog" class="modal-backdrop">
