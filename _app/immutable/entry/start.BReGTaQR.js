4e1ebaedd09b72b4f4000e8706c7e71785d7ef32
saadeghi <saadeghi@users.noreply.github.com>
2025-10-10 23:44:08 +0000

    deploy: 71d387e4fe117a53c48574092745bea66f685fd2

--- _app/immutable/entry/start.BReGTaQR.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/uRy5a78R.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.a_jjiuf-.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DP8iiVOJ.js";export{o as load_css,r as start};
