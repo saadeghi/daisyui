32e919e1bb73a3320fddf6d5381a1ad6699d65fd
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-10-11 01:43:07 +0300

    feat: icon-only drawer sidebar, new variants `is-drawer-open` and `is-drawer-close`

--- packages/docs/src/lib/mdsvex/layout-components.svelte
+++ packages/docs/src/lib/mdsvex/layout-components.svelte
@@ -340,7 +340,56 @@
                         data-tip={$t("CSS variable")}
                       >
                         <span class="badge badge-soft badge-info badge-sm whitespace-nowrap">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            class="size-3"
+                          >
+                            <g
+                              stroke-linejoin="round"
+                              stroke-linecap="round"
+                              stroke-width="2"
+                              fill="none"
+                              stroke="currentColor"
+                            >
+                              <path d="M10.1 2.18a9.93 9.93 0 0 1 3.8 0"></path>
+                              <path d="M17.6 3.71a9.95 9.95 0 0 1 2.69 2.7"></path>
+                              <path d="M21.82 10.1a9.93 9.93 0 0 1 0 3.8"></path>
+                              <path d="M20.29 17.6a9.95 9.95 0 0 1-2.7 2.69"></path>
+                              <path d="M13.9 21.82a9.94 9.94 0 0 1-3.8 0"></path>
+                              <path d="M6.4 20.29a9.95 9.95 0 0 1-2.69-2.7"></path>
+                              <path d="M2.18 13.9a9.93 9.93 0 0 1 0-3.8"></path>
+                              <path d="M3.71 6.4a9.95 9.95 0 0 1 2.7-2.69"></path>
+                              <circle cx="12" cy="12" r="1"></circle>
+                            </g>
+                          </svg>
                           {@html $t("CSS variable")}
+                        </span>
+                      </div>
+                    {/if}
+                    {#if type == "variant"}
+                      <div
+                        class="tooltip cursor-help before:text-[0.6875rem]"
+                        data-tip={$t("Use like variant:utility to apply a utility conditionally")}
+                      >
+                        <span class="badge badge-soft badge-sm whitespace-nowrap">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            class="size-3"
+                          >
+                            <g
+                              stroke-linejoin="round"
+                              stroke-linecap="round"
+                              stroke-width="2"
+                              fill="none"
+                              stroke="currentColor"
+                            >
+                              <path d="M9.183 6.117a6 6 0 1 0 4.511 3.986"></path>
+                              <path d="M14.813 17.883a6 6 0 1 0 -4.496 -3.954"></path>
+                            </g>
+                          </svg>
+                          {@html $t("Variant")}
                         </span>
                       </div>
                     {/if}
