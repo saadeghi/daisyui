9585008f9d21b08ced1c48a94c0084237ae5245b
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-11-02 21:37:04 +0330

    docs

--- packages/docs/src/routes/(routes)/blueprint/+page.svelte
+++ packages/docs/src/routes/(routes)/blueprint/+page.svelte
@@ -138,6 +138,7 @@
   title="daisyUI and Tailwind CSS MCP server"
   formatTitle={false}
   desc="daisyUI and Tailwind CSS MCP server to generate Tailwind CSS code, Convert Figma to Tailwind CSS"
+  img="https://img.daisyui.com/images/blueprint.webp"
 />
 
 <svelte:head>
@@ -153,7 +154,7 @@
   <div>
     <div class="inline-block">
       <div class="me-6 -mb-2 self-end text-end font-mono text-[0.625rem] text-blue-600">
-        Version 1.0
+        Version 1.1
       </div>
       <div class="mb-4 inline-grid grid-cols-[1rem_1fr_1rem] grid-rows-[1rem_1fr_1rem]">
         <div class="border-s-2 border-t-2 border-blue-600 [grid-area:1/1/2/2]"></div>
