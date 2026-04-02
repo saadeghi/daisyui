06b43667980bf7eb1020c3be426274aa5d09b52a
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2026-04-02 09:14:29 +0300

    docs

--- packages/docs/src/components/TopBanner.svelte
+++ packages/docs/src/components/TopBanner.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <!-- Without timer -->
-{#if true && !$page.url.pathname.startsWith("/blueprint/")}
+{#if false && !$page.url.pathname.startsWith("/blueprint/")}
   <div class="bg-base-100 flex justify-center rounded-sm">
     <a
       href="/blueprint/"
@@ -65,45 +65,35 @@
   </div>
 {/if}
 <!-- With timer -->
-<!-- {#if true && !$page.url.pathname.startsWith("/store/")} -->
-{#if false}
+{#if true}
   <div class="bg-base-100 flex justify-center rounded-sm">
     <a
       data-sveltekit-preload-data
-      href="/store/nexus/"
+      href="/store/daisyui-charts/"
       class="alert border-base-300 hover:bg-base-200 bg-base-100 flex w-full justify-center rounded-none border-x-0 border-t-0 p-2 text-center text-xs shadow-none transition-colors"
     >
       <p class="leading-relaxed [text-wrap:balance]">
         <span class="text-base-content/70">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 14 16"
-            class="mx-1 inline-block size-4 -rotate-12 align-middle"
-          >
-            <g fill="currentColor">
-              <path
-                d="M13 4h-1.38c.19-.33.33-.67.36-.91.06-.67-.11-1.22-.52-1.61C11.1 1.1 10.65 1 10.1 1h-.11c-.53.02-1.11.25-1.53.58-.42.33-.73.72-.97 1.2-.23-.48-.55-.88-.97-1.2-.42-.32-1-.58-1.53-.58h-.03c-.56 0-1.06.09-1.44.48-.41.39-.58.94-.52 1.61.03.23.17.58.36.91H1.98c-.55 0-1 .45-1 1v3h1v5c0 .55.45 1 1 1h9c.55 0 1-.45 1-1V8h1V5c0-.55-.45-1-1-1H13zm-4.78-.88c.17-.36.42-.67.75-.92.3-.23.72-.39 1.05-.41h.09c.45 0 .66.11.8.25s.33.39.3.95c-.05.19-.25.61-.5 1h-2.9l.41-.88v.01zM4.09 2.04c.13-.13.31-.25.91-.25.31 0 .72.17 1.03.41.33.25.58.55.75.92L7.2 4H4.3c-.25-.39-.45-.81-.5-1-.03-.56.16-.81.3-.95l-.01-.01zM7 12.99H3V8h4v5-.01zm0-6H2V5h5v2-.01zm5 6H8V8h4v5-.01zm1-6H8V5h5v2-.01z"
-              >
-              </path>
-            </g>
-          </svg>
-          Happy New Year! Use code
+          <span class="bg-warning text-warning-content rounded-selector px-2 font-semibold">
+            NEW
+          </span>
+          <span class="font-bold">daisyUI Charts</span> – Use code
           <code
             class="decoration-success font-mono tracking-wide underline decoration-wavy underline-offset-4"
           >
-            NEXUS26
+            CHART30
           </code>
-          and get 26% discount on Nexus Dashboard Template!
+          to get 30% discount
           <span class="inline-block w-2"></span>
           <Countdown
-            from={new Date("2026-01-06T00:00:00.000000Z").toLocaleString("en-GB", dateFormat)}
+            from={new Date("2026-04-13T00:00:00.000000Z").toLocaleString("en-GB", dateFormat)}
             dateFormat="DD/MM/YYYY, HH:mm:ss"
           >
             {#snippet children({ remaining })}
               {#if remaining.done === false}
                 <span class="border-base-content/20 rounded-full border border-dashed px-2 py-1">
                   <date
-                    datetime={new Date("2025-04-19T01:01:00.000000Z").toLocaleString(
+                    datetime={new Date("2026-04-13T00:00:00.000000Z").toLocaleString(
                       "en-GB",
                       dateFormat,
                     )}
