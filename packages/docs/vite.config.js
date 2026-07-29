fb48b9bc456fd7932f8d6baae642a424cea0e563
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2026-07-29 05:03:09 +0300

    remove rollup-plugin-visualizer

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -2,24 +2,11 @@ import path from "node:path"
 import { defineConfig } from "vite"
 import { sveltekit } from "@sveltejs/kit/vite"
 import tailwindcss from "@tailwindcss/vite"
-import { visualizer } from "rollup-plugin-visualizer"
-
-const analyzeBundle = process.env.DOCS_ANALYZE === "true"
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     sveltekit(),
-    ...(analyzeBundle
-      ? [
-          visualizer({
-            emitFile: true,
-            filename: "stats.html",
-            template: "treemap",
-            brotliSize: true,
-          }),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
