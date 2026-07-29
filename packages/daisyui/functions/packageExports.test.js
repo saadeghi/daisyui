a804cf70cbf4691d0534371089b7b30a7b64e98e
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2026-07-29 14:55:45 +0300

    fix: missing import for nested layers

--- packages/daisyui/functions/packageExports.test.js
+++ packages/daisyui/functions/packageExports.test.js
@@ -1,4 +1,9 @@
 import { expect, test } from "bun:test"
+import packageJson from "../package.json"
+
+test("package includes the nestCssLayers runtime dependency", () => {
+  expect(packageJson.files).toContain("functions/nestCssLayers.js")
+})
 
 for (const subpath of [
   "daisyui/theme",
